cdb4d509000768eccc53f87d12e13218724181e9
chandrika31 <udhayachandrikamurugan94@gmail.com>
2019-05-21 11:06:28 +0530

    header changes and godown data table expandable icon changes

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { LoginService } from '../login/login.service';
+import {  Observable } from 'rxjs';
+import { Pipe, PipeTransform } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -9,12 +11,30 @@ import { LoginService } from '../login/login.service';
 export class HeaderComponent implements OnInit {
   isValidUser: boolean;
   loggedUsername: string;
-  timer: any;
+  countDown;
+  timer = 1800;
+  tick = 1000;
   constructor(private loginService: LoginService) { }
 
   ngOnInit() {
+    // this.countDown = Observable.timer(0, this.tick)
+    //   .take(this.timer)
+    //   .map(() => --this.timer
+    //   )
     this.isValidUser = (this.loginService.getUsername() !== undefined && this.loginService.getUsername() !== '') ? true : false;
     this.loggedUsername = this.loginService.getUsername();
+  }
+  
+  
+}
+@Pipe({
+  name: 'formatTime'
+})
+export class FormatTimePipe implements PipeTransform {
+
+  transform(value: number): string {
+    const minutes: number = Math.floor(value / 60);
+    return ('00' + minutes).slice(-2) + ':' + ('00' + Math.floor(value - minutes * 60)).slice(-2);
   }
 
 }
