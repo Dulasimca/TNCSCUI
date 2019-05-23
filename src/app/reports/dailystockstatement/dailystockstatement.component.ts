b3fa902ae721515334511e61c34212ffb542b4d2
chandrika31 <udhayachandrikamurugan94@gmail.com>
2019-05-23 14:11:13 +0530

    daily stock statement logic updated

--- src/app/reports/dailystockstatement/dailystockstatement.component.ts
+++ src/app/reports/dailystockstatement/dailystockstatement.component.ts
@@ -13,91 +13,103 @@ import { HttpParams } from '@angular/common/http';
 export class DailyStockStatementComponent implements OnInit {
   dailyStockDataCoulmns: any;
   dailyStockData: any;
+  treeData: any[];
   fromDate: Date;
   toDate: Date;
   itemCodes: any = [];
   ITCODE1: any;
   ITCODE2: any;
+  totalRecords: number;
+  loading: boolean;
 
   constructor(private tableConstants: TableConstants, private restApiService: RestAPIService) { }
 
   ngOnInit() {
+    let tempArray = [];
+    this.treeData = [];
     this.dailyStockDataCoulmns = this.tableConstants.DailyStockStatement;
+    this.loading = true;
     this.restApiService.get(PathConstants.DAILY_STOCK_STATEMENT_ITEM_MASTER).subscribe(itemCodes => {
       if (itemCodes !== undefined) {
-        // this.itemCodes.push(itemCodes);
-        // for (let c = 0; c < itemCodes.length; c++) {
-        //   this.ITCODE1 = itemCodes[c].ITCode;
-        //   this.ITCODE2 = itemCodes[c + 1].ITCode;
-        //   c++;
-        this.ITCODE1 = 'IT009'; this.ITCODE2 = 'IT013';
-       let params = new HttpParams().set('ITCode1', this.ITCODE1).append('ITCode2', this.ITCODE2);
-    this.restApiService.getByParameters(PathConstants.DAILY_STOCK_STATEMENT, params).subscribe((response: any[]) => {
-      let treeData = [];
-      let childNode: TreeNode;
-      let regionChildNode: TreeNode;
-      let regionData = [];
-      let godownData = [];
-      response.forEach(x => {
-        let list = x.ListItems;
-        for (let i = 0; i < list.length; i++) {
-          let godownList = list[i].ListItems;
-          for (let i = 0; i < godownList.length; i++) {
-            regionChildNode = { 'data': {
-              'Name': godownList[i].Name,
-              'OpeningBalance': godownList[i].OpeningBalance,
-              "ClosingBalance": godownList[i].ClosingBalance,
-                "TotalReceipt": godownList[i].TotalReceipt,
-                "Receipt": parseInt(godownList[i].OpeningBalance, 0) + parseInt(godownList[i].TotalReceipt, 0),
-                "IssueSales": godownList[i].IssueSales,
-                "IssueOthers": godownList[i].IssueOthers,
-                "TotalIssue": parseInt(godownList[i].IssueSales, 0) + parseInt(godownList[i].IssueOthers, 0),
-                "CSBalance": godownList[i].CSBalance,
-                "PhycialBalance": godownList[i].PhycialBalance,
-                "Shortage": godownList[i].Shortage,
-            }}
-            godownData.push(regionChildNode);
-          }
-          childNode = { 'data': {
-            'Name': list[i].Name,
-            'OpeningBalance': list[i].OpeningBalance,
-            "ClosingBalance": list[i].ClosingBalance,
-              "TotalReceipt": list[i].TotalReceipt,
-              "Receipt": parseInt(list[i].OpeningBalance, 0) + parseInt(list[i].TotalReceipt, 0),
-              "IssueSales": list[i].IssueSales,
-              "IssueOthers": list[i].IssueOthers,
-              "TotalIssue": parseInt(list[i].IssueSales, 0) + parseInt(list[i].IssueOthers, 0),
-              "CSBalance": list[i].CSBalance,
-              "PhycialBalance": list[i].PhycialBalance,
-              "Shortage": list[i].Shortage,
-          }, "children": godownData}
-          regionData.push(childNode);
-        godownData = [];
-                 }
-        treeData.push(Object.assign({},
-          {
-            "data": {
-              "Name": x.Name,
-              "OpeningBalance": x.OpeningBalance,
-              "ClosingBalance": x.ClosingBalance,
-              "TotalReceipt": x.TotalReceipt,
-              "Receipt": parseInt(x.OpeningBalance, 0) + parseInt(x.TotalReceipt, 0),
-              "IssueSales": x.IssueSales,
-              "IssueOthers": x.IssueOthers,
-              "TotalIssue": parseInt(x.IssueSales, 0) + parseInt(x.IssueOthers, 0),
-              "CSBalance": x.CSBalance,
-              "PhycialBalance": x.PhycialBalance,
-              "Shortage": x.Shortage,
-            },
-            "children": regionData,
-          },
-        ));
-        regionData = [];
-      });
-      this.dailyStockData = treeData;
-       });
+        for (let c = 0; c < itemCodes.length; c++) {
+          this.ITCODE1 = itemCodes[c].ITCode;
+          this.ITCODE2 = itemCodes[c + 1].ITCode;
+          c = c + 1;
+          let params = new HttpParams().set('ITCode1', this.ITCODE1).append('ITCode2', this.ITCODE2);
+          this.restApiService.getByParameters(PathConstants.DAILY_STOCK_STATEMENT, params).subscribe((response: any[]) => {
+            let childNode: TreeNode;
+            let regionChildNode: TreeNode;
+            let regionData = [];
+            let godownData = [];
+            response.forEach(x => {
+              let list = x.ListItems;
+              for (let i = 0; i < list.length; i++) {
+                let godownList = list[i].ListItems;
+                for (let i = 0; i < godownList.length; i++) {
+                  regionChildNode = {
+                    'data': {
+                      'Name': godownList[i].Name,
+                      'OpeningBalance': godownList[i].OpeningBalance,
+                      "ClosingBalance": godownList[i].ClosingBalance,
+                      "TotalReceipt": godownList[i].TotalReceipt,
+                      "Receipt": parseInt(godownList[i].OpeningBalance, 0) + parseInt(godownList[i].TotalReceipt, 0),
+                      "IssueSales": godownList[i].IssueSales,
+                      "IssueOthers": godownList[i].IssueOthers,
+                      "TotalIssue": parseInt(godownList[i].IssueSales, 0) + parseInt(godownList[i].IssueOthers, 0),
+                      "CSBalance": godownList[i].CSBalance,
+                      "PhycialBalance": godownList[i].PhycialBalance,
+                      "Shortage": godownList[i].Shortage,
+                    }
+                  }
+                  godownData.push(regionChildNode);
+                }
+                childNode = {
+                  'data': {
+                    'Name': list[i].Name,
+                    'OpeningBalance': list[i].OpeningBalance,
+                    "ClosingBalance": list[i].ClosingBalance,
+                    "TotalReceipt": list[i].TotalReceipt,
+                    "Receipt": parseInt(list[i].OpeningBalance, 0) + parseInt(list[i].TotalReceipt, 0),
+                    "IssueSales": list[i].IssueSales,
+                    "IssueOthers": list[i].IssueOthers,
+                    "TotalIssue": parseInt(list[i].IssueSales, 0) + parseInt(list[i].IssueOthers, 0),
+                    "CSBalance": list[i].CSBalance,
+                    "PhycialBalance": list[i].PhycialBalance,
+                    "Shortage": list[i].Shortage,
+                  }, "children": godownData
+                }
+                regionData.push(childNode);
+                godownData = [];
+              }
+              this.treeData.push(
+                {
+                  "data": {
+                    "Name": x.Name,
+                    "OpeningBalance": x.OpeningBalance,
+                    "ClosingBalance": x.ClosingBalance,
+                    "TotalReceipt": x.TotalReceipt,
+                    "Receipt": parseInt(x.OpeningBalance, 0) + parseInt(x.TotalReceipt, 0),
+                    "IssueSales": x.IssueSales,
+                    "IssueOthers": x.IssueOthers,
+                    "TotalIssue": parseInt(x.IssueSales, 0) + parseInt(x.IssueOthers, 0),
+                    "CSBalance": x.CSBalance,
+                    "PhycialBalance": x.PhycialBalance,
+                    "Shortage": x.Shortage,
+                  },
+                  "children": regionData,
+                },
+              );
+              regionData = [];
+            });
+            for (let i = 0; i < this.treeData.length; i++) {
+              tempArray.push({ 'data': this.treeData[i].data, 'children': this.treeData[i].children });
+            }
+            console.log('temp', tempArray);
+            this.treeData = [];
+            this.dailyStockData = tempArray;
+          });
+        }
       }
-    // }
-  })
-      }
+    })
+  }
 }
