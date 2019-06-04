49d1f15ca120ad5fbb2267da7e72e9efadc7e3f8
Subashvishal <subashvishal64@gmail.com>
2019-06-04 17:19:59 +0530

    Issue data table included with scroll

--- src/app/Documents/issue-receipt/issue-receipt.component.ts
+++ src/app/Documents/issue-receipt/issue-receipt.component.ts
@@ -6,10 +6,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./issue-receipt.component.css']
 })
 export class IssueReceiptComponent implements OnInit {
-
+data: any;
+col: any;
   constructor() { }
 
   ngOnInit() {
+    this.col = [ { field: 'vin', header: 'Vin' },
+    {field: 'year', header: 'Year' },
+    { field: 'brand', header: 'Brand' },
+    { field: 'color', header: 'Color' },
+    { field: 'vin', header: 'Vin' },
+    {field: 'year', header: 'Year' },
+    { field: 'brand', header: 'Brand' },
+    { field: 'color', header: 'Color' }];
+    this.data = [ {'vin': 'ajc', 'year': 'adkahd', 'brand': 'jhgdjagd', 'color': 'hjjfa'},
+    {'vin': 'ajc', 'year': 'adkahd', 'brand': 'jhgdjagd', 'color': 'hjjfa'},
+    {'vin': 'ajc', 'year': 'adkahd', 'brand': 'jhgdjagd', 'color': 'hjjfa'},
+    {'vin': 'ajc', 'year': 'adkahd', 'brand': 'jhgdjagd', 'color': 'hjjfa'},
+    {'vin': 'ajc', 'year': 'adkahd', 'brand': 'jhgdjagd', 'color': 'hjjfa'},
+    {'vin': 'ajc', 'year': 'adkahd', 'brand': 'jhgdjagd', 'color': 'hjjfa'},
+    {'vin': 'ajc', 'year': 'adkahd', 'brand': 'jhgdjagd', 'color': 'hjjfa'},
+  ];
   }
 
 }
