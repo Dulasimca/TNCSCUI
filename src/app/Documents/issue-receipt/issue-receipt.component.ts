d76c1fe6fe7caef8c22eb3358f15b6767c7aa8ca
subashvishal <43237843+subashvishal@users.noreply.github.com>
2019-06-06 17:16:21 +0530

    Issue Receipt design done

--- src/app/Documents/issue-receipt/issue-receipt.component.ts
+++ src/app/Documents/issue-receipt/issue-receipt.component.ts
@@ -8,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class IssueReceiptComponent implements OnInit {
 data: any;
 col: any;
+itemCol: any;
+itemData: any;
   constructor() { }
 
   ngOnInit() {
@@ -21,6 +23,26 @@ col: any;
     {'Delivery Order No': '2', 'Issue Memo No': 'B', 'Delivery Order': 'F','Delivery': 'j'},
     {'Delivery Order No': '3', 'Issue Memo No': 'C', 'Delivery Order': 'G','Delivery': 'k'},
     {'Delivery Order No': '4', 'Issue Memo No': 'D', 'Delivery Order': 'H','Delivery': 'l'}
+  ];
+
+  this.itemCol = [
+    { field: 'Stack No.', header:'StackNo' },
+    { field: 'Item Description', header:'ItemDesc' },
+    { field: 'Packing Type', header:'PackingType' },
+    { field: 'No. of packing', header:'No Packing' },
+    { field: 'Wmt Type', header:'WmtType' },
+    { field: 'Gross Wt', header:'GrossWt' },
+    { field: 'Moisture', header:'Moisture' },
+    { field: 'Scheme', header:'Scheme' },
+    { field: 'Net Wt', header:'NetWT' }
+  ]
+  this.itemData = [
+    { 'Stack No.': '1','Item Description':'A','Packing Type':'','No. of packing':'','Wmt Type': '','Gross Wt': '','Moisture': '','Scheme': '','Net Wt': '' },
+    { 'Stack No.': '2','Item Description':'B','Packing Type':'','No. of packing':'','Wmt Type': '','Gross Wt': '','Moisture': '','Scheme': '','Net Wt': '' },
+    { 'Stack No.': '3','Item Description':'C','Packing Type':'','No. of packing':'','Wmt Type': '','Gross Wt': '','Moisture': '','Scheme': '','Net Wt': '' },
+    { 'Stack No.': '4','Item Description':'D','Packing Type':'','No. of packing':'','Wmt Type': '','Gross Wt': '','Moisture': '','Scheme': '','Net Wt': '' },
+    { 'Stack No.': '5','Item Description':'E','Packing Type':'','No. of packing':'','Wmt Type': '','Gross Wt': '','Moisture': '','Scheme': '','Net Wt': '' },
+    { 'Stack No.': '6','Item Description':'F','Packing Type':'','No. of packing':'','Wmt Type': '','Gross Wt': '','Moisture': '','Scheme': '','Net Wt': '' },
   ];
   }
 
