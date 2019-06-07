ff23482d8cfb3bd305d7c9bc77794457ba4f519f
subashvishal <43237843+subashvishal@users.noreply.github.com>
2019-06-08 00:21:38 +0530

    All 4 EntryPage design Completed

--- src/app/Documents/truck-receipt/truck-receipt.component.ts
+++ src/app/Documents/truck-receipt/truck-receipt.component.ts
@@ -6,10 +6,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./truck-receipt.component.css']
 })
 export class TruckReceiptComponent implements OnInit {
-
+itemData: any;
+itemCol: any;
   constructor() { }
 
   ngOnInit() {
+    this.itemCol = [
+      { field: 'Stack No.', header:'StackNo' },
+      { field: 'Item Description', header:'ItemDesc' },
+      { field: 'Packing Type', header:'PackingType' },
+      { field: 'No. of packing', header:'No Packing' },
+      { field: 'Wmt Type', header:'WmtType' },
+      { field: 'Gross Wt', header:'GrossWt' },
+      { field: 'Net Wt', header:'NetWT' },
+      { field: 'Moisture', header:'Moisture' },
+      { field: 'Scheme', header:'Scheme' }
+    ]
+    this.itemData = [
+      { 'Stack No.': '1','Item Description':'A','Packing Type':'','No. of packing':'','Wmt Type': '','Gross Wt': '','Moisture': '','Scheme': '','Net Wt': '' },
+      { 'Stack No.': '2','Item Description':'B','Packing Type':'','No. of packing':'','Wmt Type': '','Gross Wt': '','Moisture': '','Scheme': '','Net Wt': '' },
+      { 'Stack No.': '3','Item Description':'C','Packing Type':'','No. of packing':'','Wmt Type': '','Gross Wt': '','Moisture': '','Scheme': '','Net Wt': '' },
+      { 'Stack No.': '4','Item Description':'D','Packing Type':'','No. of packing':'','Wmt Type': '','Gross Wt': '','Moisture': '','Scheme': '','Net Wt': '' },
+      { 'Stack No.': '5','Item Description':'E','Packing Type':'','No. of packing':'','Wmt Type': '','Gross Wt': '','Moisture': '','Scheme': '','Net Wt': '' },
+      { 'Stack No.': '6','Item Description':'F','Packing Type':'','No. of packing':'','Wmt Type': '','Gross Wt': '','Moisture': '','Scheme': '','Net Wt': '' },
+    ];
   }
 
 }
