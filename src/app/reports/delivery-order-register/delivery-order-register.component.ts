05a82102c22d89a046fad5fcd168af6ce2d099de
subashvishal <43237843+subashvishal@users.noreply.github.com>
2019-06-12 14:12:25 +0530

    Receipts Included

--- src/app/reports/delivery-order-register/delivery-order-register.component.ts
+++ src/app/reports/delivery-order-register/delivery-order-register.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { TableConstants } from 'src/app/constants/tableconstants';
 import { RestAPIService } from 'src/app/shared-services/restAPI.service';
+import { DatePipe } from '@angular/common';
+import { RoleBasedService } from 'src/app/common/role-based.service';
+import { SelectItem } from 'primeng/api';
+import { HttpParams } from '@angular/common/http';
+import { PathConstants } from 'src/app/constants/path.constants';
+import { ExcelService } from 'src/app/shared-services/excel.service';
 
 @Component({
   selector: 'app-delivery-order-register',
@@ -10,11 +16,46 @@ import { RestAPIService } from 'src/app/shared-services/restAPI.service';
 export class DeliveryOrderRegisterComponent implements OnInit {
   DeliveryReceiptRegCols: any;
   DeliveryReceiptRegData: any;
+  fromDate: any;
+  toDate: any;
+  isViewDisabled: any;
+  isActionDisabled: any;
+  data: any;
+  g_cd='548';
+  deliveryOptions: SelectItem[];
+  deliveryName: string;
 
-  constructor(private tableConstants: TableConstants, private restAPIService: RestAPIService) { }
+  constructor(private tableConstants: TableConstants, private datePipe: DatePipe, private excelService: ExcelService, private restAPIService: RestAPIService, private roleBasedService: RoleBasedService) { }
 
   ngOnInit() {
+    this.isViewDisabled = this.isActionDisabled = true;
     this.DeliveryReceiptRegCols = this.tableConstants.DeliveryMemoRegisterReport;
+    this.data = this.roleBasedService.getInstance();
+    console.log('data', this.data);
   }
 
+  onSelect() {
+    let options = [];
+    if (this.fromDate !== undefined && this.fromDate !== '' && this.toDate !== undefined && this.toDate !== '' && this.g_cd !== '' && this.g_cd !== undefined) {
+      this.isViewDisabled = false;
+    }
+    this.data.forEach(element => {
+      options.push({ 'label': element.Name, 'value': element.Code });
+      this.deliveryOptions = options;
+    });
+  }
+
+  onView() {
+    const params = new HttpParams().set('Fdate', this.datePipe.transform(this.fromDate, 'MM-DD-YYYY')).append('ToDate', this.datePipe.transform(this.toDate, 'MM-DD-YYYY')).append('GCode', this.g_cd);
+    this.restAPIService.getByParameters(PathConstants.STOCK_DELIVERY_ORDER_REPORT, params).subscribe(res => {
+      if (res !== undefined) {
+        this.isActionDisabled = false;
+      }
+      console.log('res', res);
+    })
+  }
+
+  exportAsXLSX():void{
+    this.excelService.exportAsExcelFile(this.DeliveryReceiptRegData, 'Truck_Memo',this.DeliveryReceiptRegCols);
+}
 }
--- src/app/reports/stock-issue-register/stock-issue-register.component.ts
+++ src/app/reports/stock-issue-register/stock-issue-register.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { TableConstants } from 'src/app/constants/tableconstants';
 import { RestAPIService } from 'src/app/shared-services/restAPI.service';
+import { PathConstants } from 'src/app/constants/path.constants';
+import { HttpParams } from '@angular/common/http';
+import { SelectItem } from 'primeng/api';
+import { DatePipe } from '@angular/common';
+import { RoleBasedService } from 'src/app/common/role-based.service';
+import { ExcelService } from 'src/app/shared-services/excel.service';
 
 @Component({
   selector: 'app-stock-issue-register',
@@ -8,12 +14,48 @@ import { RestAPIService } from 'src/app/shared-services/restAPI.service';
   styleUrls: ['./stock-issue-register.component.css']
 })
 export class StockIssueRegisterComponent implements OnInit {
-stockIssueRegCols: any;
-stockIssueRegData: any;
-  constructor(private tableConstants: TableConstants, private restAPIService: RestAPIService) { }
+  stockIssueRegCols: any;
+  stockIssueRegData: any;
+  fromDate: any;
+  toDate: any;
+  isViewDisabled: any;
+  isActionDisabled: any;
+  data: any;
+  g_cd= '548';
+  godownOptions: SelectItem[];
+  godownName: string;
+
+  constructor(private tableConstants: TableConstants, private datePipe: DatePipe, private excelService: ExcelService, private restAPIService: RestAPIService, private roleBasedService: RoleBasedService) { }
 
   ngOnInit() {
+    this.isViewDisabled = this.isActionDisabled = true;
     this.stockIssueRegCols = this.tableConstants.StockIssueRegisterReport;
+    this.data = this.roleBasedService.getInstance();
+    console.log('data', this.data);
   }
 
+  onSelect() {
+    let options = [];
+    if (this.fromDate !== undefined && this.fromDate !== '' && this.toDate !== undefined && this.toDate !== '' && this.g_cd !== '' && this.g_cd !== undefined) {
+      this.isViewDisabled = false;
+    }
+    this.data.forEach(element => {
+      options.push({ 'label': element.Name, 'value': element.Code });
+      this.godownOptions = options;
+    });
+  }
+
+  onView() {
+    const params = new HttpParams().set('Fdate', this.datePipe.transform(this.fromDate, 'MM-DD-YYYY')).append('ToDate', this.datePipe.transform(this.toDate, 'MM-DD-YYYY')).append('GCode', this.g_cd);
+    this.restAPIService.getByParameters(PathConstants.STOCK_ISSUE_REGISTER_REPORT, params).subscribe(res => {
+      if (res !== undefined) {
+        this.isActionDisabled = false;
+      }
+      console.log('res', res);
+    })
+  }
+
+  exportAsXLSX():void{
+    this.excelService.exportAsExcelFile(this.stockIssueRegData, 'Truck_Memo',this.stockIssueRegCols);
+}
 }
