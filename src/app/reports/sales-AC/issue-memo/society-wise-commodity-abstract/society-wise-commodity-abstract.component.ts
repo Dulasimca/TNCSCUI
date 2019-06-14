b3300f55c4ccc261f1e35ca8931b2bbbb316efea
chandrika31 <udhayachandrikamurugan94@gmail.com>
2019-06-14 18:38:02 +0530

    society wise dropdown added

--- src/app/reports/sales-AC/issue-memo/society-wise-commodity-abstract/society-wise-commodity-abstract.component.ts
+++ src/app/reports/sales-AC/issue-memo/society-wise-commodity-abstract/society-wise-commodity-abstract.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SelectItem } from 'primeng/api';
 
 @Component({
   selector: 'app-society-wise-commodity-abstract',
@@ -6,19 +7,63 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./society-wise-commodity-abstract.component.css']
 })
 export class SocietyWiseCommodityAbstractComponent implements OnInit {
-  brkUpViewDisplay: boolean = false;
-  schemeWiseBrkUp: boolean = false;
-  schemeAbstractBrkUp: boolean = false;
+  canShowMenu: boolean;
+  showCommodityAbstract: boolean = false;
+  showCommodityBreakup: boolean = false;
+  showSchemeCommodityBreakup: boolean = false;
+  showSchemeAbstract: boolean = false;
+  abstractOptions: SelectItem[];
+  a_cd: string;
 
   constructor() { }
 
   ngOnInit() {
   }
 
-  viewBreakup(event) {
-    const id = event.target.attributes.id;
-    console.log('id', id);
-    this.brkUpViewDisplay = true;
+  onSelect(selectedItem) {
+    switch (selectedItem) {
+      case 'customer':
+        break;
+      case 'godown':
+        break;
+      case 'abstract':
+        this.abstractOptions = [{ 'label': 'Society Wise Commodity Breakup', 'value': 'society_c_a' },
+        { 'label': 'Society Wise Date Wise Commodity Abstract', 'value': 'date_c_b' },
+        { 'label': 'Society Wise Scheme Wise Commodity Breakup', 'value': 'scheme_c_b' },
+        { 'label': 'Society Wise Scheme Wise Commodity Abstract', 'value': 'scheme_c_a' }];
+        this.showPane();
+        break;
+    }
+  }
+
+  showPane() {
+    switch (this.a_cd) {
+      case 'society_c_a':
+        this.showCommodityAbstract = true;
+        this.showCommodityBreakup = false;
+        this.showSchemeAbstract = false;
+        this.showSchemeCommodityBreakup = false;
+        break;
+      case 'date_c_b':
+        this.showCommodityAbstract = false;
+        this.showCommodityBreakup = true;
+        this.showSchemeAbstract = false;
+        this.showSchemeCommodityBreakup = false;
+        break;
+      case 'scheme_c_b':
+        this.showCommodityAbstract = false;
+        this.showCommodityBreakup = false;
+        this.showSchemeAbstract = false;
+        this.showSchemeCommodityBreakup = true;
+        break;
+      case 'scheme_c_a':
+        this.showCommodityAbstract = false;
+        this.showCommodityBreakup = false;
+        this.showSchemeAbstract = true;
+        this.showSchemeCommodityBreakup = false;
+        break;
+    }
+
   }
 
 
