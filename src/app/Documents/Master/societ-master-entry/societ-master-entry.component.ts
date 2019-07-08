c3a3b5c1629dbc4e14f724fbe0f310b088f80b45
subashvishal <43237843+subashvishal@users.noreply.github.com>
2019-07-08 12:49:14 +0530

    Document master included

--- src/app/Documents/Master/societ-master-entry/societ-master-entry.component.ts
+++ src/app/Documents/Master/societ-master-entry/societ-master-entry.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { TableConstants } from 'src/app/constants/tableconstants';
+import { AuthService } from 'src/app/shared-services/auth.service';
 
 @Component({
   selector: 'app-societ-master-entry',
@@ -6,10 +8,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./societ-master-entry.component.css']
 })
 export class SocietMasterEntryComponent implements OnInit {
+  SocietyMasterEntryCols: any;
+  SocietyMasterEntryData: any;
+  data?:any;
+  g_cd: any;
+  isViewDisabled: boolean;
+  isActionDisabled: boolean;
+  canShowMenu: boolean;
 
-  constructor() { }
+  constructor(private tableConstants: TableConstants, private authService: AuthService) { }
 
   ngOnInit() {
+    this.canShowMenu = (this.authService.isLoggedIn()) ? this.authService.isLoggedIn() : false;
+    this.SocietyMasterEntryCols = this.tableConstants.SocietyMasterEntry;
+
   }
 
 }
