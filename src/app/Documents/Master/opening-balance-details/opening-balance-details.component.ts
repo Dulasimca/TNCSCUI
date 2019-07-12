ea2cf22d5860f9c77b99506cdaa019f612024124
chandrika <udhayachandrikam31@gmail.com>
2019-07-12 17:39:43 +0530

    logic in opening stack

--- src/app/Documents/Master/opening-balance-details/opening-balance-details.component.ts
+++ src/app/Documents/Master/opening-balance-details/opening-balance-details.component.ts
@@ -37,22 +37,62 @@ export class OpeningBalanceDetailsComponent implements OnInit {
   viewPane: boolean;
   selectedRow: any;
   msgs: any;
+  g_cd: any;
   roleId: any;
-  allowSelect: boolean;
+  showErr: boolean = false;
+  rData: any = [];
+  gdata: any = [];
+  loggedInGCode: any;
+  loggedInRCode: any;
+  rName: any;
 
-  constructor(private authService: AuthService, private roleBasedService: RoleBasedService,
+  constructor(private authService: AuthService, private roleBasedService: RoleBasedService, private restApiService: RestAPIService,
     private restAPIService: RestAPIService, private tableConstants: TableConstants, private messageService: MessageService) { }
 
   ngOnInit() {
     this.canShowMenu = (this.authService.isLoggedIn()) ? this.authService.isLoggedIn() : false;
-    this.data = this.roleBasedService.getInstance();
     this.roleId = this.authService.getUserAccessible().roleId;
-    setTimeout(() => {
-      this.allowSelect = (this.roleId != 3) ? true : false;
-      this.godownName = this.data.rgData[1].GName;
-      this.gCode = this.data.rgData[1].GCode;
-      this.rCode = this.data.rgData[0].RCode;
-    }, 1200);
+    this.loggedInGCode = this.authService.getUserAccessible().gCode;
+    this.loggedInRCode = this.authService.getUserAccessible().rCode;
+    this.restApiService.get(PathConstants.GODOWN_MASTER).subscribe((res: any) => {
+      let gList;
+      if(res !== undefined) {
+        if (this.roleId === '3') {
+          this.data = res.filter(x => {
+            if (x.Code === this.loggedInRCode) {
+              gList = x.list;
+              this.rData.push({ 'RName': x.Name, 'RCode': x.Code })
+              this.rCode = this.rData[0].RCode;
+              this.rName = this.rData[0].RName;
+            }
+            gList.filter(y => {
+              if (y.GCode === this.loggedInGCode) {
+                this.gdata.push({'GName': y.Name, 'GCode': y.GCode})
+                this.godownName = this.gdata[0].GName;
+                this.gCode = this.gdata[0].GCode;
+              }
+            })
+          })
+        } else if (this.roleId === '2') {
+          this.data = res.filter(x => {
+            if (x.Code === this.loggedInRCode) {
+              gList = x.list;
+              this.rData.push({ 'RName': x.Name, 'RCode': x.Code })
+            }
+            gList.forEach(y => {
+                this.gdata.push({'GName': y.Name, 'GCode': y.GCode})
+            })
+          })
+        } else {
+          this.data = res.forEach(x => {
+          gList = x.list;
+          gList.forEach(y => {
+            this.gdata.push({'GName': y.Name, 'GCode': y.GCode})
+        })
+          });
+        }
+      }
+    });
     if (this.commodityOptions === undefined) {
       this.restAPIService.get(PathConstants.ITEM_MASTER).subscribe(data => {
         if (data !== undefined) {
@@ -66,12 +106,27 @@ export class OpeningBalanceDetailsComponent implements OnInit {
     }
   }
 
+  calculateCS() {
+    if (this.BookBalanceWeight  !== undefined && this.PhysicalBalanceWeight !== undefined) {
+      if (this.BookBalanceWeight < this.PhysicalBalanceWeight) {
+        this.showErr = true;
+        this.PhysicalBalanceWeight = this.CumulitiveShortage = null;
+      } else {
+        this.showErr = false;
+        this.CumulitiveShortage = this.BookBalanceWeight - this.PhysicalBalanceWeight;
+      }
+    } else {
+      this.CumulitiveShortage = 0;
+    }
+   
+  }
+
   onSelect(selectedItem) {
     let godownSelection = [];
     switch (selectedItem) {
       case 'gd':
-        if (this.data.godownData !== undefined) {
-          this.data.godownData.forEach(x => {
+        if (this.gdata !== undefined) {
+          this.gdata.forEach(x => {
             godownSelection.push({ 'label': x.GName, 'value': x.GCode });
             this.godownOptions = godownSelection;
           });
@@ -101,7 +156,7 @@ export class OpeningBalanceDetailsComponent implements OnInit {
       if (selectedItem !== null) {
         this.openingBalanceData = this.openingBalanceData.filter(x => { return x.ITDescription === selectedItem.label });
         if (this.openingBalanceData.length === 0) {
-          this.messageService.add({ key: 't-err', severity: 'warn', summary: 'Warn Message', detail: 'Record not found!' });
+          this.messageService.add({ key: 't-warn', severity: 'warn', summary: 'Warn Message', detail: 'Record not found!' });
         }
       } else {
         this.openingBalanceData = this.opening_balance;
@@ -134,7 +189,7 @@ export class OpeningBalanceDetailsComponent implements OnInit {
 
   onView() {
     this.openingBalanceData = [];
-    const params = new HttpParams().set('ObDate', '04' + '/' + '01' + '/' + this.Year.value).append('GCode', this.gCode);
+    const params = new HttpParams().set('ObDate', '04' + '/' + '01' + '/' + this.Year.value).append('GCode', (this.gCode !== undefined) ? this.gCode : this.g_cd.value);
     this.restAPIService.getByParameters(PathConstants.OPENING_BALANCE_MASTER_GET, params).subscribe((res: any) => {
       if (res !== undefined && res !== null && res.length !== 0) {
         this.viewPane = true;
@@ -144,14 +199,19 @@ export class OpeningBalanceDetailsComponent implements OnInit {
         this.opening_balance = this.openingBalanceData.slice(0);
       } else {
         this.viewPane = false;
-        this.messageService.add({ key: 't-err', severity: 'error', summary: 'Error Message', detail: 'Something went wrong!' });
+        this.messageService.add({ key: 't-warn', severity: 'warn', summary: 'Warn Message', detail: 'Record Not Found!' });
       }
     })
   }
 
+  onClear() {
+    this.BookBalanceBags = this.BookBalanceWeight = this.PhysicalBalanceBags = this.PhysicalBalanceWeight =
+    this.c_cd = this.commodityCd = this.CumulitiveShortage = this.Year = this.g_cd = null;
+  }
+
   onSave() {
     const params = {
-      'GodownCode': this.gCode,
+      'GodownCode': (this.gCode !== undefined) ? this.gCode : this.g_cd.value,
       'CommodityCode': (this.c_cd.value !== null && this.c_cd.value !== undefined) ? this.c_cd.value : this.commodityCd,
       'ObDate': this.Year.value,
       'BookBalanceBags': this.BookBalanceBags,
@@ -163,11 +223,12 @@ export class OpeningBalanceDetailsComponent implements OnInit {
     };
     this.restAPIService.post(PathConstants.OPENING_BALANCE_MASTER_POST, params).subscribe(res => {
       if (res) {
-        this.messageService.add({ key: 't-err', severity: 'success', summary: 'Success Message', detail: 'Saved Successfully!' });
+        this.messageService.add({ key: 't-success', severity: 'success', summary: 'Success Message', detail: 'Saved Successfully!' });
       } else {
-        this.messageService.add({ key: 't-err', severity: 'error', summary: 'Error Message', detail: 'Something went wrong!' });
+        this.messageService.add({ key: 't-err', severity: 'error', summary: 'Error Message', detail: 'Please try again!' });
       }
     })
+    this.onClear();
   }
 
 }
