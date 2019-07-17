79e4f9575b3fc9b7f5fa086e44a07cc338687b3a
chandrika <udhayachandrikam31@gmail.com>
2019-07-17 17:17:33 +0530

    timer in dailystock

--- src/app/reports/dailystockstatement/dailystockstatement.component.ts
+++ src/app/reports/dailystockstatement/dailystockstatement.component.ts
@@ -133,7 +133,7 @@ export class DailyStockStatementComponent implements OnInit {
             this.loading = false;
             this.totalRecords = tempArray.length;
             this.dailyStockData = tempArray;
-            }, 1300);
+            }, 1700);
             this.filterArray = tempArray;
           }, (err: HttpErrorResponse) => {
             if (err.status === 0) {
