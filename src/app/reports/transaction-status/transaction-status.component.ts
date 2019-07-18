bb780db4498dbc0009db05c85ef7ce1a48fabdca
subashvishal <43237843+subashvishal@users.noreply.github.com>
2019-07-18 16:58:32 +0530

    Transaction status and truck transit issues resolved

--- src/app/reports/transaction-status/transaction-status.component.ts
+++ src/app/reports/transaction-status/transaction-status.component.ts
@@ -19,14 +19,16 @@ export class TransactionStatusComponent implements OnInit {
   g_cd: any;
   gCode: any;
   data: any;
-  SRDate: any;
+  // Docdate: any;
   godownName: SelectItem[];
-  disableOkButton: boolean =true;
+  disableOkButton: boolean = true;
   Docdate: any;
-  Receipt: any;
-  Issues: any;
-  Transaction: any;
-  CbStatement: any;
+  userid: any;
+  remarks: any;
+  Receipt: boolean;
+  Issues: boolean;
+  Transfer: boolean;
+  CB: boolean;
   Transaction_Status: any;
   maxDate: Date;
   viewPane: boolean;
@@ -35,13 +37,14 @@ export class TransactionStatusComponent implements OnInit {
   godownOptions: SelectItem[];
   canShowMenu: boolean;
 
-  constructor(private tableConstants: TableConstants ,private messageService: MessageService, private restAPIService: RestAPIService ,private datepipe: DatePipe ,private roleBasedService: RoleBasedService , private authService : AuthService) { }
+  constructor(private tableConstants: TableConstants, private messageService: MessageService, private restAPIService: RestAPIService, private datepipe: DatePipe, private roleBasedService: RoleBasedService, private authService: AuthService) { }
 
   ngOnInit() {
     this.canShowMenu = (this.authService.isLoggedIn()) ? this.authService.isLoggedIn() : false;
     this.data = this.roleBasedService.getInstance();
     this.TransactionStatusCols = this.tableConstants.TransactionStatus;
     this.maxDate = new Date();
+    this.userid = JSON.parse(this.authService.getCredentials());
   }
 
   onSelect() {
@@ -52,34 +55,70 @@ export class TransactionStatusComponent implements OnInit {
         options.push({ 'label': x.GName, 'value': x.GCode });
         this.godownOptions = options;
       });
-  }
+    }
   }
 
   onRowSelect(event) {
     this.disableOkButton = false;
-    this.selectedRow = event.SRDate;
+    this.selectedRow = event.Docdate;
   }
 
   showSelectedData() {
     this.viewPane = false;
-    this.SRDate = this.selectedRow.SRDate;
+    this.Docdate = this.selectedRow.DocdDate;
+    // this.Docdate = this.Docdate
     this.Receipt = this.selectedRow.Receipt;
     this.Issues = this.selectedRow.Issues;
-    this.Transaction = this.selectedRow.Transaction;
-    this.CbStatement = this.selectedRow.CbStatement;
+    this.Transfer = this.selectedRow.Transfer;
+    this.CB = this.selectedRow.CB;
+    this.remarks = this.selectedRow.remarks;
+    this.userid = this.userid; 
   }
 
-  onView(){
+  // onView(){
+  //   this.TransactionStatusData = [];
+  //   // this.viewPane = true;
+  //   const params = new HttpParams().set('SRDate', this.datepipe.transform(this.viewDate, 'MM/dd/yyyy')).append('GCode', (this.gCode !== undefined) ? this.gCode : this.g_cd.value);
+  //   this.restAPIService.getByParameters(PathConstants.TRANSACTION_STATUS, params).subscribe((res: any) => {
+  //     if (res !== undefined && res !== null && res.length !== 0) {
+  //       this.viewPane = true;
+  //       let sno = 0;
+  //       this.TransactionStatusCols = this.tableConstants.TransactionStatus;
+  //       this.TransactionStatusData = res;
+  //       this.TransactionStatusData.forEach(x => x.GodownName = (this.godownName !== undefined) ? this.godownName : this.g_cd.label);
+  //       this.SRDate = this.datepipe.transform(this.SRDate, 'dd-MM-yyyy');
+  //       this.Transaction_Status = this.TransactionStatusData.slice(0);
+  //     } else {
+  //       this.viewPane = false;
+  //       this.messageService.add({ key: 't-warn', severity: 'warn', summary: 'Warn Message', detail: 'Record Not Found!' });
+  //     }
+  //   })
+  // }
+
+
+  onView() {
     this.TransactionStatusData = [];
-    this.viewPane = true;
-    const params = new HttpParams().set('sValue', this.datepipe.transform(this.viewDate, 'MM/dd/yyyy')).append('GCode', (this.gCode !== undefined) ? this.gCode: this.g_cd.value);
-    this.restAPIService.getByParameters(PathConstants.STACK_OPENING_ENTRY_REPORT_GET, params).subscribe((res: any) => {
+    // this.viewPane = true;
+    const params = new HttpParams().set('sValue', this.datepipe.transform(this.viewDate, 'MM/dd/yyyy')).append('GCode', (this.gCode !== undefined) ? this.gCode : this.g_cd.value);
+    this.restAPIService.getByParameters(PathConstants.TRANSACTION_STATUS, params).subscribe((res: any) => {
       if (res !== undefined && res !== null && res.length !== 0) {
         this.viewPane = true;
-        this.TransactionStatusCols = this.tableConstants.StackCardOpeningEntryReport;
-        this.TransactionStatusData = res;
+        // let sno = 0;
+        this.TransactionStatusCols = this.tableConstants.TransactionStatus;
+        res.forEach(x => {
+          // sno += 1;
+          this.TransactionStatusData.push({
+            'Receipt': x.Receipt,
+            'Issues': x.Issues,
+            'Transfer': x.Transfer,
+            'CB': x.CB,
+            'Docdate': x.Docdate,
+            'remarks': x.remarks,
+            'userid': x.userid,
+          })
+        });
         this.TransactionStatusData.forEach(x => x.GodownName = (this.godownName !== undefined) ? this.godownName : this.g_cd.label);
-        this.SRDate = this.datepipe.transform(this.SRDate, 'dd-MM-yyyy');
+        this.Docdate = this.datepipe.transform(this.Docdate, 'dd-MM-yyyy');
         this.Transaction_Status = this.TransactionStatusData.slice(0);
       } else {
         this.viewPane = false;
@@ -89,20 +128,21 @@ export class TransactionStatusComponent implements OnInit {
   }
 
   onClear() {
-    this.Receipt = this.Issues = this.Transaction = this.CbStatement =
-      this.g_cd = this.SRDate = null;
+    this.Receipt = this.Issues = this.Transfer = this.CB = this.g_cd = this.Docdate = this.remarks = null;
   }
 
-  onSave(){
+  onSave() {
     const params = {
-      'GodownCode': (this.gCode !== undefined) ? this.gCode : this.g_cd.value,
-      'SRDate': this.datepipe.transform(this.SRDate, 'MM/dd/yyyy'),
-      'Receipt': this.Receipt,
-      'Issues': this.Issues,
-      'Transaction': this.Transaction,
-      'CbStatement': this.CbStatement
+      'Gcode': (this.gCode !== undefined) ? this.gCode : this.g_cd.value,
+      'Docdate': this.datepipe.transform(this.Docdate, 'MM/dd/yyyy'),
+      'Receipt': (this.Receipt !== undefined && this.Receipt !== null) ? true : false,
+      'Issues': (this.Issues !== undefined && this.Issues !== null) ? true : false,
+      'Transfer': (this.Transfer !== undefined && this.Transfer !== null) ? true : false,
+      'CB': (this.CB !== undefined && this.CB !== null) ? true : false,
+      'remarks': (this.remarks !== undefined && this.remarks !== null) ? this.remarks : 'No Remarks',
+      'userid': this.userid.user,
     };
-    this.restAPIService.post(PathConstants.STACK_OPENING_ENTRY_REPORT_POST, params).subscribe(res => {
+    this.restAPIService.post(PathConstants.TRANSACTION_STATUS, params).subscribe(res => {
       if (res) {
         this.messageService.add({ key: 't-success', severity: 'success', summary: 'Success Message', detail: 'Saved Successfully!' });
       } else {
@@ -115,6 +155,6 @@ export class TransactionStatusComponent implements OnInit {
     })
     this.onClear();
   }
-  }
+}
 
-  
+
