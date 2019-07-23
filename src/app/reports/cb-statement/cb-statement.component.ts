4de0c22909fca191b9692733a722e9c5c665c778
dulasimca <dulasimca@gmail.com>
2019-07-23 11:23:06 +0530

    Changes added

--- src/app/reports/cb-statement/cb-statement.component.ts
+++ src/app/reports/cb-statement/cb-statement.component.ts
@@ -5,12 +5,8 @@ import { TableConstants } from 'src/app/constants/tableconstants';
 import { PathConstants } from 'src/app/constants/path.constants';
 import { HttpErrorResponse } from '@angular/common/http';
 import { Router } from '@angular/router';
-import { Observable } from 'rxjs/Observable';
-import 'rxjs/add/observable/from';
-import 'rxjs/Rx';
-import * as Rx from 'rxjs';
-import * as _ from 'lodash';
-import { groupBy, mergeMap, toArray, flatMap, concatMap } from 'rxjs/operators';
+import { of, Observable, from } from 'rxjs';
+import { groupBy, mergeMap, toArray, flatMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-cb-statement',
@@ -36,35 +32,33 @@ export class CBStatementComponent implements OnInit {
     this.rowGroupMetadata = {};
     this.canShowMenu = (this.authService.isLoggedIn()) ? this.authService.isLoggedIn() : false;
     this.column = this.tableConstants.CBStatementColumns;
-    this.loading = true;
+    // this.loading = true;
     this.restApiService.get(PathConstants.CB_STATEMENT_REPORT).subscribe(response => {
       if (response.Table !== undefined && response.Table !== null && response.Table.length !== 0) {
         this.cbData = response.Table;
+        this.record = response.Table.slice(0);
+        let totalData = response.Table1;
         this.cbData.forEach(record => {
-          let boiledRiceTotal: number = ((record.BOILED_RICE_A !== null && record.BOILED_RICE_A !== undefined) ? (record.BOILED_RICE_A * 1) : 0) +
+          let boiledRiceTotal = ((record.BOILED_RICE_A !== null && record.BOILED_RICE_A !== undefined) ? (record.BOILED_RICE_A * 1) : 0) +
             ((record.BOILED_RICE_A_HULLING !== null && record.BOILED_RICE_A_HULLING !== undefined) ? (record.BOILED_RICE_A_HULLING * 1) : 0) +
             ((record.BOILED_RICE_C_HULLING !== null && record.BOILED_RICE_C_HULLING !== undefined) ? (record.BOILED_RICE_C_HULLING * 1) : 0) +
             ((record.BOILED_RICE_COMMON !== null && record.BOILED_RICE_COMMON !== undefined) ? (record.BOILED_RICE_COMMON * 1) : 0);
           record.boiledRice = (boiledRiceTotal !== 0) ? boiledRiceTotal.toFixed(3) : boiledRiceTotal;
-          record.boiledRice = (record.boiledRice * 1);
           let rawRiceTotal = ((record.RAW_RICE_A !== null && record.RAW_RICE_A !== undefined) ? (record.RAW_RICE_A * 1) : 0) +
             ((record.RAW_RICE_A_HULLING !== null && record.RAW_RICE_A_HULLING !== undefined) ? (record.RAW_RICE_A_HULLING * 1) : 0) +
             ((record.RAW_RICE_COM_HULLING !== null && record.RAW_RICE_COM_HULLING !== undefined) ? (record.RAW_RICE_COM_HULLING * 1) : 0) +
             ((record.RAW_RICE_COMMON !== null && record.RAW_RICE_COMMON !== undefined) ? (record.RAW_RICE_COMMON * 1) : 0);
           record.rawRice = (rawRiceTotal !== 0) ? rawRiceTotal.toFixed(3) : rawRiceTotal;
-          record.rawRice = (record.rawRice * 1);
           let kanadaToorDhallTotal = ((record.Candian_Yellow_lentil_TD !== null && record.Candian_Yellow_lentil_TD !== undefined) ?
             record.Candian_Yellow_lentil_TD * 1 : 0) + ((record.YELLOW_LENTAL_US !== null && record.YELLOW_LENTAL_US !== undefined) ?
               record.YELLOW_LENTAL_US * 1 : 0);
           record.kanadaToorDhall = (kanadaToorDhallTotal !== 0) ? kanadaToorDhallTotal.toFixed(3) : kanadaToorDhallTotal;
-          record.kanadaToorDhall = (record.kanadaToorDhall * 1);
           let toorDhallTotal = ((record.TOOR_DHALL !== null && record.TOOR_DHALL !== undefined) ?
             record.TOOR_DHALL * 1 : 0) + ((record.TUR_ARUSHA !== null && record.TUR_ARUSHA !== undefined) ?
               record.TUR_ARUSHA * 1 : 0) + ((record.TUR_LEMON !== null && record.TUR_LEMON !== undefined) ?
                 record.TUR_LEMON * 1 : 0) + ((record.LIARD_LENTIL_GREEN !== null && record.LIARD_LENTIL_GREEN !== undefined) ?
                   record.LIARD_LENTIL_GREEN * 1 : 0);
           record.toorDhall = (toorDhallTotal !== 0) ? toorDhallTotal.toFixed(3) : toorDhallTotal;
-          record.toorDhall = (record.toorDhall * 1);
           let uridDhallTotal = ((record.URAD_FAQ !== null && record.URAD_FAQ !== undefined) ?
             record.URAD_FAQ * 1 : 0) + ((record.URAD_SQ !== null && record.URAD_SQ !== undefined) ?
               record.URAD_SQ * 1 : 0) + ((record.URID_DHALL !== null && record.URID_DHALL !== undefined) ?
@@ -73,70 +67,136 @@ export class CBStatementComponent implements OnInit {
                     record.URID_DHALL_SPLIT * 1 : 0) + ((record.URID_DHALL_SQ !== null && record.URID_DHALL_SQ !== undefined) ?
                       record.URID_DHALL_SQ * 1 : 0);
           record.uridDhall = (uridDhallTotal !== 0) ? uridDhallTotal.toFixed(3) : uridDhallTotal;
-          record.uridDhall = (record.uridDhall * 1);
           let palmoilTotal = ((record.PALMOLIEN_OIL !== null && record.PALMOLIEN_OIL !== undefined) ?
             record.PALMOLIEN_OIL * 1 : 0) + ((record.PALMOLIEN_POUCH !== null && record.PALMOLIEN_POUCH !== undefined) ?
               record.PALMOLIEN_POUCH * 1 : 0);
           record.palmoil = (palmoilTotal !== 0) ? palmoilTotal.toFixed(3) : palmoilTotal;
-          record.palmoil = (record.palmoil * 1);
           let cementTotal = ((record.CEMENT_IMPORTED !== null && record.CEMENT_IMPORTED !== undefined) ?
             record.CEMENT_IMPORTED * 1 : 0) + ((record.CEMENT_REGULAR !== null && record.CEMENT_REGULAR !== undefined) ?
               record.CEMENT_REGULAR * 1 : 0) + ((record.AMMA_CEMENT !== null && record.AMMA_CEMENT !== undefined) ?
               record.AMMA_CEMENT * 1 : 0);
           record.cement = (cementTotal !== 0) ? cementTotal.toFixed(3) : cementTotal;
-          record.cement = (record.cement * 1);
           let totalRice = boiledRiceTotal + rawRiceTotal;
           let totalDhall = toorDhallTotal + kanadaToorDhallTotal;
           record.totalRice = (totalRice !== 0) ? totalRice.toFixed(3) : totalRice;
-          record.totalRice = (record.totalRice * 1);
           record.totalDhall = (totalDhall !== 0) ? totalDhall.toFixed(3) : totalDhall;
-          record.totalDhall = (record.totalDhall * 1);
           record.WHEAT = (record.WHEAT !== 0) ? record.WHEAT.toFixed(3) : record.WHEAT;
-          record.WHEAT = (record.WHEAT * 1);
           record.SUGAR = (record.SUGAR !== 0) ? record.SUGAR.toFixed(3) : record.SUGAR;
-          record.SUGAR = (record.SUGAR * 1);
         });
-        this.cbData.splice(this.cbData.length, 0, '');
-        
-let groupedData;
-Rx.Observable.from(this.cbData)
-      .groupBy((x: any) => x.RGNAME) // using groupBy from Rxjs
-      .flatMap(group => group.toArray())// GroupBy dont create a array object so you have to flat it
-      .map(g => {// mapping 
-        return {
-          RGNAME: g[0].RGNAME,//take the first name because we grouped them by name
-          TNCSCapacity: _.sumBy(g, 'TNCSCapacity'),
-          boiledRice: _.sumBy(g, 'boiledRice'), // using lodash to sum quantity
-          rawRice: _.sumBy(g, 'rawRice'),
-          totalRice: _.sumBy(g, 'totalRice'),
-          SUGAR: _.sumBy(g, 'SUGAR'),
-          WHEAT: _.sumBy(g, 'WHEAT'),
-          toorDhall: _.sumBy(g, 'toorDhall'),
-          kanadaToorDhall: _.sumBy(g, 'kanadaToorDhall'),
-          totalDhall: _.sumBy(g, 'totalDhall'),
-          uridDhall: _.sumBy(g, 'uridDhall'),
-          palmoil: _.sumBy(g, 'palmoil'),
-          cement: _.sumBy(g, 'cement')
-        }
-      })
-      .toArray() //.toArray because I guess you want to loop on it with ngFor      
-      .do(sum => sum) // just for debug
-      .subscribe(d => groupedData = d);
-      let index = 0;
-      let item;
-      for(let i = 0; i < this.cbData.length; i++) {
-        if (this.cbData[i].RGNAME !== groupedData[index].RGNAME) {
-            item = { TNCSName: 'TOTAL', TNCSCapacity: groupedData[index].TNCSCapacity, boiledRice: (groupedData[index].boiledRice * 1).toFixed(3), rawRice: (groupedData[index].rawRice * 1).toFixed(3),
-            totalRice: (groupedData[index].totalRice * 1).toFixed(3), totalDhall: (groupedData[index].totalDhall * 1).toFixed(3), toorDhall: (groupedData[index].toorDhall * 1).toFixed(3), kanadaToorDhall: (groupedData[index].kanadaToorDhall * 1).toFixed(3),
-          kanadaToorDhallTotal: (groupedData[index].kanadaToorDhallTotal * 1).toFixed(3), SUGAR: (groupedData[index].SUGAR * 1).toFixed(3), WHEAT: (groupedData[index].WHEAT * 1).toFixed(3), cement: (groupedData[index].cement * 1).toFixed(3),
-        uridDhall: (groupedData[index].uridDhall * 1).toFixed(3), palmoil: groupedData[index].palmoil };
-          this.cbData.splice(i, 0, item);
-          index += 1;
-           console.log(this.cbData.length);
+        // let reduceArr = [];
+        // data.forEach(x => reduceArr.push(x.RGNAME));
+        // var map = reduceArr.reduce(function (item, index) {
+        //   item[index] = (item[index] || 0) + 1;
+        //   return item;
+        // }, {});
+        // let count = 0;
+        // let k = 0;
+        // let ind;
+        // let mapIndex = 0;
+        // let totalBoiledRice;
+        // let totalCement;
+        // let totalKanadaDhall;
+        // let totalToorDhall;
+        // let totalPalmOil;
+        // let totalRawRice;
+        // let totalUridDhallTotal;
+        // let totalSugar;
+        // let totalWheat;
+        // let findIndex = 0;
+        // let grandTotal = 0;
+        // while ((count > 0 || count === 0) && count < data.length - 1) {
+        //   let name = (data[count] !== undefined && data[count].RGNAME !== undefined) ? data[count].RGNAME : data[count + 1].RGNAME;
+        //   if (data[count].RGNAME === data[count + 1].RGNAME) { ind = map[name] + count; } else { ind = map[name] + count + 1; }
+        //   mapIndex = map[name];
+        //   if (data[findIndex] !== undefined && data[findIndex].RGNAME === name) {
+        //     let i = totalData.findIndex(item => item.RGNAME === data[findIndex].RGNAME);
+        //     totalBoiledRice = ((totalData[i].BOILED_RICE_A !== null && totalData[i].BOILED_RICE_A !== undefined) ?
+        //       totalData[i].BOILED_RICE_A * 1 : totalData[i].BOILED_RICE_A) + ((totalData[i].BOILED_RICE_A_HULLING !== null && totalData[i].BOILED_RICE_A_HULLING !== undefined) ?
+        //         totalData[i].BOILED_RICE_A_HULLING * 1 : totalData[i].BOILED_RICE_A_HULLING) + ((totalData[i].BOILED_RICE_COMMON !== null && totalData[i].BOILED_RICE_COMMON !== undefined) ?
+        //           totalData[i].BOILED_RICE_COMMON * 1 : totalData[i].BOILED_RICE_COMMON) + ((totalData[i].BOILED_RICE_C_HULLING !== null && totalData[i].BOILED_RICE_C_HULLING !== undefined) ?
+        //             totalData[i].BOILED_RICE_C_HULLING * 1 : totalData[i].BOILED_RICE_C_HULLING);
+        //     totalBoiledRice = (totalBoiledRice !== 0) ? totalBoiledRice.toFixed(3) : totalBoiledRice;
+        //     totalCement = ((totalData[i].CEMENT_IMPORTED !== null && totalData[i].CEMENT_IMPORTED !== undefined) ?
+        //       totalData[i].CEMENT_IMPORTED * 1 : totalData[i].CEMENT_IMPORTED) + ((totalData[i].CEMENT_REGULAR !== null && totalData[i].CEMENT_REGULAR !== undefined) ?
+        //         totalData[i].CEMENT_REGULAR * 1 : totalData[i].CEMENT_REGULAR);
+        //     totalCement = (totalCement !== 0) ? totalCement.toFixed(3) : totalCement;
+        //     totalKanadaDhall = ((totalData[i].Candian_Yellow_lentil_TD !== null && totalData[i].Candian_Yellow_lentil_TD !== undefined) ?
+        //       totalData[i].Candian_Yellow_lentil_TD * 1 : 0) + ((totalData[i].YELLOW_LENTAL_US !== null && totalData[i].YELLOW_LENTAL_US !== undefined) ?
+        //         totalData[i].YELLOW_LENTAL_US * 1 : 0);
+        //     totalKanadaDhall = (totalKanadaDhall !== 0) ? totalKanadaDhall.toFixed(3) : totalKanadaDhall;
+        //     totalToorDhall = ((totalData[i].TOOR_DHALL !== null && totalData[i].TOOR_DHALL !== undefined) ?
+        //       totalData[i].TOOR_DHALL * 1 : 0) + ((totalData[i].TUR_ARUSHA !== null && totalData[i].TUR_ARUSHA !== undefined) ?
+        //         totalData[i].TUR_ARUSHA * 1 : 0) + ((totalData[i].TUR_LEMON !== null && totalData[i].TUR_LEMON !== undefined) ?
+        //           totalData[i].TUR_LEMON * 1 : 0) + ((totalData[i].LIARD_LENTIL_GREEN !== null && totalData[i].LIARD_LENTIL_GREEN !== undefined) ?
+        //             totalData[i].LIARD_LENTIL_GREEN * 1 : 0);
+        //     totalToorDhall = (totalToorDhall !== 0) ? totalToorDhall.toFixed(3) : totalToorDhall;
+        //     totalPalmOil = ((totalData[i].PALMOLIEN_OIL !== null && totalData[i].PALMOLIEN_OIL !== undefined) ?
+        //       totalData[i].PALMOLIEN_OIL * 1 : 0) + ((totalData[i].PALMOLIEN_POUCH !== null && totalData[i].PALMOLIEN_POUCH !== undefined) ?
+        //         totalData[i].PALMOLIEN_POUCH * 1 : 0);
+        //     totalPalmOil = (totalPalmOil !== 0) ? totalPalmOil.toFixed(3) : totalPalmOil;
+        //     totalRawRice = ((totalData[i].RAW_RICE_A !== null && totalData[i].RAW_RICE_A !== undefined) ? (totalData[i].RAW_RICE_A * 1) : 0) +
+        //       ((totalData[i].RAW_RICE_A_HULLING !== null && totalData[i].RAW_RICE_A_HULLING !== undefined) ? (totalData[i].RAW_RICE_A_HULLING * 1) : 0) +
+        //       ((totalData[i].RAW_RICE_COM_HULLING !== null && totalData[i].RAW_RICE_COM_HULLING !== undefined) ? (totalData[i].RAW_RICE_COM_HULLING * 1) : 0) +
+        //       ((totalData[i].RAW_RICE_COMMON !== null && totalData[i].RAW_RICE_COMMON !== undefined) ? (totalData[i].RAW_RICE_COMMON * 1) : 0);
+        //     totalRawRice = (totalRawRice !== 0) ? totalRawRice.toFixed(3) : totalRawRice;
+        //     totalUridDhallTotal = ((totalData[i].URAD_FAQ !== null && totalData[i].URAD_FAQ !== undefined) ?
+        //       totalData[i].URAD_FAQ * 1 : 0) + ((totalData[i].URAD_SQ !== null && totalData[i].URAD_SQ !== undefined) ?
+        //         totalData[i].URAD_SQ * 1 : 0) + ((totalData[i].URID_DHALL !== null && totalData[i].URID_DHALL !== undefined) ?
+        //           totalData[i].URID_DHALL * 1 : 0) + ((totalData[i].URID_DHALL_FAQ !== null && totalData[i].URID_DHALL_FAQ !== undefined) ?
+        //             totalData[i].URID_DHALL_FAQ * 1 : 0) + ((totalData[i].URID_DHALL_SPLIT !== null && totalData[i].URID_DHALL_SPLIT !== undefined) ?
+        //               totalData[i].URID_DHALL_SPLIT * 1 : 0) + ((totalData[i].URID_DHALL_SQ !== null && totalData[i].URID_DHALL_SQ !== undefined) ?
+        //                 totalData[i].URID_DHALL_SQ * 1 : 0);
+        //     totalUridDhallTotal = (totalUridDhallTotal !== 0) ? totalUridDhallTotal.toFixed(3) : totalUridDhallTotal;
+        //     totalSugar = ((totalData[i].SUGAR !== null && totalData[i].SUGAR !== undefined) ?
+        //       totalData[i].SUGAR * 1 : 0);
+        //     totalSugar = (totalSugar !== 0) ? totalSugar.toFixed(3) : totalSugar;
+        //     totalWheat = ((totalData[i].WHEAT !== null && totalData[i].WHEAT !== undefined) ?
+        //       totalData[i].WHEAT * 1 : 0);
+        //     totalWheat = (totalWheat !== 0) ? totalWheat.toFixed(3) : totalWheat;
+        //     findIndex += mapIndex;
+        //   } else {
+        //     var item = { 'TNCSName': 'TOTAL', 'boiledRice': totalBoiledRice, 'cement': totalCement, 'rawRice': totalRawRice,
+        //   'SUGAR': totalSugar, 'WHEAT': totalWheat, 'toorDhall': totalToorDhall, 'kanadaToorDhall': totalKanadaDhall,
+        // 'uridDhall': totalUridDhallTotal, 'palmoil': totalPalmOil };
+        //     this.cbData.splice(ind, 0, item);
+        //     count = ind + 1;
+        //     findIndex += 1;
         //   }
-        }
-      }
-
+        // }
+        // let capacity = 0;
+        // let setFlag = false;
+        // let index = 0;
+        // let spliceIndex = 1;
+        // const arr: any[] = this.record;
+        // // this.record.forEach(x => {
+        // arr.filter(x => {
+        //   // if (index < arr.length) {
+        //     if (arr[index].RGNAME === arr[index + 1].RGNAME) {
+        //       setFlag = false;
+        //       capacity += arr[index].TNCSCapacity;
+        //       // arr.splice(0, 1);
+        //       console.log('c', capacity);
+        //       index += 1;
+        //     } else {
+        //       if (!setFlag) {
+        //         // arr.splice(0, 1);
+        //         console.log('c', capacity);
+        //         console.log('ind', index);
+        //         capacity += arr[index + 1].TNCSCapacity;
+        //         // arr.splice(0, index);
+        //         var item = { 'TNCSName': 'TOTAL', 'TNCSCapacity': capacity };
+        //         this.cbData.splice(index + spliceIndex, 0, item);
+        //         spliceIndex += 1;
+        //         index += 1;
+        //         setFlag = true;
+        //         capacity = 0;
+        //         console.log('index',index);
+        //         console.log('cb', this.cbData);
+        //       }
+        //     }
+        //   // }
+        // })
+        // })
         for (let i = 0; i < this.cbData.length; i++) {
           let rowData = this.cbData[i];
           let RGNAME = rowData.RGNAME;
@@ -152,6 +212,14 @@ Rx.Observable.from(this.cbData)
               this.rowGroupMetadata[RGNAME] = { index: i, size: 1 };
           }
         }
+        //   var result = this.record.map(o => {
+        //     var k = Object.keys(o)[0];
+        //     return {
+        //         name: k,
+        //         value: o[k]
+        //     };
+        // });
+
         this.loading = false;
       } else {
         this.loading = false;
