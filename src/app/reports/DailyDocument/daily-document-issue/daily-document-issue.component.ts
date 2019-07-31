d546d877076cd9ef1d9a0b86ad28851ccb33b6dd
subashvishal <43237843+subashvishal@users.noreply.github.com>
2019-07-31 17:56:19 +0530

    Daily Documents included and issue resolved in transaction status

--- src/app/reports/DailyDocument/daily-document-issue/daily-document-issue.component.ts
+++ src/app/reports/DailyDocument/daily-document-issue/daily-document-issue.component.ts
@@ -5,6 +5,8 @@ import { RestAPIService } from 'src/app/shared-services/restAPI.service';
 import { RoleBasedService } from 'src/app/common/role-based.service';
 import { DatePipe } from '@angular/common';
 import { AuthService } from 'src/app/shared-services/auth.service';
+import { PathConstants } from 'src/app/constants/path.constants';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-daily-document-issue',
@@ -18,22 +20,15 @@ export class DailyDocumentIssueComponent implements OnInit {
   DailyDocumentIssueData: any;
   g_cd: any;
   gCode: any;
+  rCode: any;
+  r_cd: any;
+  DocumentDate: Date;
+  roleId: any;
   gdata: any;
-  godownName: SelectItem[];
-  disableOkButton: boolean = true;
-  Docdate: Date;
+  isActionDisabled: any;
   userid: any;
-  remarks: any;
-  Receipt: boolean;
-  Issues: boolean;
-  Transfer: boolean;
-  CB: boolean;
-  Transaction_Status: any;
   maxDate: Date;
   loading: boolean;
-  viewPane: boolean;
-  selectedRow: any;
-  viewDate: Date = new Date();
   godownOptions: SelectItem[];
   canShowMenu: boolean;
 
@@ -42,12 +37,13 @@ export class DailyDocumentIssueComponent implements OnInit {
   ngOnInit() {
     this.canShowMenu = (this.authService.isLoggedIn()) ? this.authService.isLoggedIn() : false;
     this.gdata = this.roleBasedService.getInstance();
+    this.isActionDisabled = true;
+    this.roleId = JSON.parse(this.authService.getUserAccessible().roleId);
     this.DailyDocumentTotalCols = this.tableConstants.DailyDocumentTotalReport;
     this.DailyDocumentIssueCols = this.tableConstants.DailyDocumentIssue;
     this.maxDate = new Date();
     this.userid = JSON.parse(this.authService.getCredentials());
   }
-
 
   onSelect(selectedItem) {
     let godownSelection = [];
@@ -61,28 +57,45 @@ export class DailyDocumentIssueComponent implements OnInit {
           this.godownOptions.unshift({ 'label': '-select-', 'value': null, disabled: true });
         }
         break;
-       
     }
   }
 
-  onRowSelect(event) {
-    this.disableOkButton = false;
-    this.selectedRow = event.Docdate;
+  ontime() {
+    this.loading = true;
+    const params = {
+      'GodownCode': (this.g_cd.value !== null && this.g_cd.value !== undefined) ? this.g_cd.value : this.gCode,
+      'RegionCode': this.g_cd.rcode,
+      'RoleId': this.roleId,
+      'DocumentDate': this.datepipe.transform(this.DocumentDate, 'MM/dd/yyyy')
+    }
+    this.restAPIService.post(PathConstants.DAILY_DOCUMENT_ISSUE_POST, params).subscribe(res => {
+      this.DailyDocumentIssueData = res;
+      this.DailyDocumentTotalData = this.gdata
+      this.DailyDocumentTotalData.forEach(s => {
+        s.RCode = this.g_cd.rcode,
+          s.GCode = this.g_cd.value,
+          s.GName = this.g_cd.label,
+          s.RName,
+          s.NoDocument = res.length
+      })
+      let sno = 0;
+      this.DailyDocumentIssueData.forEach(data => {
+        data.DocDate = this.datepipe.transform(data.DocDate, 'dd-MM-yyyy');
+        sno += 1;
+        data.SlNo = sno;
+      })
+      if (res !== undefined && res.length !== 0) {
+        this.isActionDisabled = false;
+      } else {
+        this.loading = false;
+        this.messageService.add({ key: 't-date', severity: 'warn', summary: 'Warning!', detail: 'No record for this combination' });
+      }
+    })
   }
 
-  showSelectedData() {
-    this.viewPane = false;
-    this.Docdate = this.selectedRow.DocdDate;
-    this.Receipt = this.selectedRow.Receipt;
-    this.Issues = this.selectedRow.Issues;
-    this.Transfer = this.selectedRow.Transfer;
-    this.CB = this.selectedRow.CB;
-    this.remarks = this.selectedRow.remarks;
-    this.userid = this.userid;
+  onResetTable() {
+    this.DailyDocumentIssueData = [];
+    this.DailyDocumentTotalData = [];
+    this.isActionDisabled = true;
   }
-
-  onView() {
-    
-  }
-
 }
