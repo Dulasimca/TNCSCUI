38eee56ca2bd6cf5771140ed5530b4ecd3a61be1
chandrika <udhayachandrikam31@gmail.com>
2019-09-18 17:23:20 +0530

    truck memo commented code removed

--- src/app/Documents/truck-receipt/truck-receipt.component.ts
+++ src/app/Documents/truck-receipt/truck-receipt.component.ts
@@ -921,31 +921,8 @@ export class TruckReceiptComponent implements OnInit {
     let filepath = path + filename + ".txt";
     var w = window.open(filepath);
     w.print();
-    // this.http.get(filepath, {responseType: 'text'})
-    //   .subscribe(data => {
-    //     if(data !== null && data !== undefined) {
-    //     var doc = new jsPDF({
-    //       orientation: 'potrait',
-    //     })
-    //     doc.setFont('courier');
-    //     doc.setFontSize(9);
-    //     doc.text(data, 2, 2)
-    //     doc.save(filename + '.pdf');
-    //     this.blockScreen = false;
-    //     this.isSaveSucceed = false;
-    //     this.isViewed = false;
-    //   } else {
-    //     this.blockScreen = false;
     this.messageService.clear();
-    //     this.messageService.add({ key: 't-err', severity: StatusMessage.SEVERITY_ERROR, summary: StatusMessage.SUMMARY_ERROR, detail: StatusMessage.ErrorMessage });
-    //   } 
-    //   },(err: HttpErrorResponse) => {
-    //     this.blockScreen = false;
-    //      if (err.status === 0) {
     this.messageService.clear();
-    //       this.messageService.add({ key: 't-err', severity: StatusMessage.SEVERITY_ERROR, summary: StatusMessage.SUMMARY_ERROR, detail: StatusMessage.ErrorMessage });
-    //     }
-    //   });
   }
 
   onPrint() {
