a1e0d66057cb5fdbd0767215f2f1d3405da8a0c8
chandrika <udhayachandrikam31@gmail.com>
2019-09-21 14:26:00 +0530

    filename added in global variable

--- src/app/common/globalvariable.ts
+++ src/app/common/globalvariable.ts
@@ -1,16 +1,19 @@
 export class GolbalVariable {
+    ///Reports
     public static readonly StockDORegFilename = 'DOREG';
     public static readonly StockReceiptRegFilename = 'REREG';
     public static readonly StocTruckMemoRegFilename = 'TMREG';
     public static readonly StockIssueRegFilename = 'ISREG';
+    public static readonly QuantityACForIssue = 'ISQAC';
+    public static readonly QuantityACForReceipt = 'REQAC';
+    public static readonly StackCardDetailsReport = 'SCARD';
+    public static readonly QuantityACForReceiptScheme = '';
+    public static readonly QuantityACForTruckMemoScheme = '';
+
+    ///Documents
     public static readonly StockReceiptDocument = 'REDOC';
     public static readonly StockIssueDocument = 'ISDOC';
     public static readonly StockTruckMemoDocument = 'TMDOC';
     public static readonly DeliveryOrderDocument = 'DODOC';
     public static readonly DDChequeDocument = 'DDCHE';
-    public static readonly QuantityACForIssue = 'ISQAC';
-    public static readonly QuantityACForReceipt = 'REQAC';
-    public static readonly StackCardDetailsReport = 'DDCHE';
-    public static readonly QuantityACForReceiptScheme = '';
-    public static readonly QuantityACForTruckMemoScheme = '';
 }
