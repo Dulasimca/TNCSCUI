4355239c6fbf2e0430e83b72cd8f9f3da55c4280
chandrika <udhayachandrikam31@gmail.com>
2019-11-05 14:08:03 +0530

    added allotment module

---
+++ src/app/Allotment/allotment-details/allotment-details.component.ts
@@ -0,0 +1,171 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Dropdown, SelectItem } from 'primeng/primeng';
+import { AuthService } from 'src/app/shared-services/auth.service';
+import { DatePipe } from '@angular/common';
+import * as XLSX from 'xlsx';
+
+@Component({
+  selector: 'app-allotment-details',
+  templateUrl: './allotment-details.component.html',
+  styleUrls: ['./allotment-details.component.css']
+})
+export class AllotmentDetailsComponent implements OnInit {
+  roleId: any;
+  canShowMenu: boolean;
+  month: any;
+  monthOptions: SelectItem[];
+  yearOptions: SelectItem[];
+  year: any;
+  username: any;
+  curMonth: any;
+  AllotmentCols: any = [];
+  AllotmentData: any = [];
+  totalRecords: number;
+  @ViewChild('m') monthPanel: Dropdown;
+  @ViewChild('y') yearPanel: Dropdown;
+
+  constructor(private authService: AuthService, private datepipe: DatePipe) { }
+
+  ngOnInit() {
+    this.canShowMenu = (this.authService.isLoggedIn()) ? this.authService.isLoggedIn() : false;
+    this.roleId = JSON.parse(this.authService.getUserAccessible().roleId);
+    this.username = JSON.parse(this.authService.getCredentials());
+    this.curMonth = "0" + (new Date().getMonth() + 1);
+    this.month = this.datepipe.transform(new Date(), 'MMM');
+    this.monthOptions = [{ label: this.month, value: this.curMonth }];
+    this.year = new Date().getFullYear();
+    this.yearOptions = [{ label: this.year, value: this.year }];
+   
+  }
+  onSelect(selectedItem, type) {
+    let yearArr: any = [];
+    const range = 3;
+    switch (selectedItem) {
+      case 'y':
+        if (type === 'enter') {
+          this.yearPanel.overlayVisible = true;
+        }
+        const year = new Date().getFullYear();
+        for (let i = 0; i < range; i++) {
+          if (i === 0) {
+            yearArr.push({ 'label': (year - 1).toString(), 'value': year - 1 });
+          } else if (i === 1) {
+            yearArr.push({ 'label': (year).toString(), 'value': year });
+          } else {
+            yearArr.push({ 'label': (year + 1).toString(), 'value': year + 1 });
+          }
+        }
+        this.yearOptions = yearArr;
+        this.yearOptions.unshift({ 'label': '-select-', 'value': null, disabled: true });
+        break;
+      case 'm':
+        if (type === 'enter') {
+          this.monthPanel.overlayVisible = true;
+        }
+        this.monthOptions = [{ 'label': 'Jan', 'value': '01' },
+        { 'label': 'Feb', 'value': '02' }, { 'label': 'Mar', 'value': '03' }, { 'label': 'Apr', 'value': '04' },
+        { 'label': 'May', 'value': '05' }, { 'label': 'Jun', 'value': '06' }, { 'label': 'Jul', 'value': '07' },
+        { 'label': 'Aug', 'value': '08' }, { 'label': 'Sep', 'value': '09' }, { 'label': 'Oct', 'value': '10' },
+        { 'label': 'Nov', 'value': '11' }, { 'label': 'Dec', 'value': '12' }];
+        this.monthOptions.unshift({ 'label': '-select-', 'value': null, disabled: true });
+        break;
+  }
+}
+
+uploadData(event) {
+  let filesData = event.target.files;
+  this.parseExcel(filesData[0]);
+  // console.log(filesData[0]);
+}
+
+parseExcel(file) {  
+  let reader = new FileReader();  
+  reader.onload = (e)=> {  
+    let data = (<any>e.target).result;  
+    let workbook = XLSX.read(data, {  
+      type: 'binary'  
+    });  
+    workbook.SheetNames.forEach((function(sheetName) {  
+      // convert execl data into JSON 
+      let columns: Array<any> = [];
+      let XL_row_object = XLSX.utils.sheet_to_json(workbook.Sheets[sheetName]);  
+      let headers = get_header_row(workbook.Sheets[sheetName]);
+      console.log('headers', headers);
+      headers.forEach(c => {
+        this.AllotmentCols.push({ header: c, field: c });
+      })
+      console.log('cols', this.AllotmentCols);
+      // let headers_from_arr = XL_row_object.splice(5, 1);
+      // for (var i in headers_from_arr) {
+      //   columns.push({ header: i, field: i });
+      // } 
+    
+      
+    //  console.log('hdrfrm', headers_from_arr);
+      // for(let i = 6; i <= XL_row_object.length; i++){
+      //   columns.forEach(x => {
+      //     let field = x.field;
+      //     if(x[field] === XL_row_object[i][field]) {
+      //       XL_row_object[i][field] = 
+      //     }
+         
+      //   })
+      // }
+      // let arr3 = XL_row_object.map((item, i) => Object.assign({}, item, headers_from_arr[i]));
+      // console.log('arr', arr3);
+      // for(let i = 6; i < XL_row_object.length; i++) {
+      //   let obj: any = [];
+      //   obj = XL_row_object[i];
+      //   for(let j = 0; j < obj.length; j++) {
+      //     if(obj[j] === headers_from_arr[j]) {
+      //       console.log('working');
+      //     }
+      //   }
+      // }
+      let object = Object.keys(XL_row_object).reduce((acc, k) => {
+        let key = XL_row_object[k];
+        acc[key] = acc[key] || [];
+        acc[key].push(k);
+      //  console.log('acc', acc);
+        return acc;
+      }, {}); 
+      for (let key in XL_row_object) {
+        // console.log('keys', XL_row_object[key]);
+      }
+             let json_object = JSON.stringify(XL_row_object);  
+
+      // bind the parse excel file data to Grid  
+      let data = JSON.parse(json_object); 
+      this.totalRecords = data.length; 
+      this.AllotmentData = data;
+      console.log('data', data);
+    //  console.log('json', data);
+    }).bind(this), this);  
+  };  
+
+  reader.onerror = function(ex) {  
+    console.log(ex);  
+  };  
+  reader.readAsBinaryString(file);  
+};  
+}
+
+function get_header_row(sheet) {
+  var headers = [];
+  var range = XLSX.utils.decode_range(sheet['!ref']);
+  var C, R = range.s.r; /* start in the first row */
+  // var R = 9;
+  /* walk every column in the range */
+  for(C = range.s.c; C <= range.e.c; ++C) {
+      var cell = sheet[XLSX.utils.encode_cell({c:C, r:R})] /* find the cell in the first row */
+
+      var hdr = "UNKNOWN " + C; // <-- replace with your desired default 
+      if(cell && cell.t) hdr = XLSX.utils.format_cell(cell);
+
+      headers.push(hdr);
+  }
+  console.log('header', headers);
+  return headers;
+}
+
+
