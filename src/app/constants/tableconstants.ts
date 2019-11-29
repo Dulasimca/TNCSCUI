52cbbaa3802c8bb8cdb881bc5339f73f06f8d07a
chandrika <udhayachandrikam31@gmail.com>
2019-11-29 12:46:05 +0530

    stack card details report addditional changes added

--- src/app/constants/tableconstants.ts
+++ src/app/constants/tableconstants.ts
@@ -113,6 +113,7 @@ export class TableConstants {
   ProcessToG2GIssueCols: any[];
   ProcessToG2GCols: any[];
   QuantityACIssueDetailsCommodity: any[];
+  StackCardDocDetailsCols: any[];
 
   constructor() {
     this.DailyStockStatement = [
@@ -1362,6 +1363,16 @@ export class TableConstants {
       { header: 'Purchase Return', field: 'PurchaseReturn' },
       { header: 'Total Issues', field: 'Total Issues' },
       { header: 'Closing Balance', field: 'ClosingBalance' },
+    ];
+
+    this.StackCardDocDetailsCols = [
+      { header: 'S.No', field: 'SlNo' },
+      { header: 'Doc.No', field: 'DocNo' },
+      { header: 'Party Name', field: 'ReceivedFrom'},
+      { header: 'Lorry No.', field: 'LorryNo' },
+      { header: 'Doc.created Date', field: 'CreatedDate' },
+      { header: 'Bags', field: 'NOOfPACKING'},
+      { header: 'Quantity', field: 'NETWT' },
     ];
   }
 }
