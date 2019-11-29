781157d1810ab1d2b6da946fdce314d66d8a7633
chandrika <udhayachandrikam31@gmail.com>
2019-11-29 17:28:26 +0530

    decimal directive modifications and cash receipt doc changes

--- src/app/directives/DecimalForAmount.directive.ts
+++ src/app/directives/DecimalForAmount.directive.ts
@@ -4,22 +4,21 @@ import { Directive, ElementRef, HostListener } from '@angular/core';
   selector: '[appDecimalAmount]'
 })
 export class AmountDecimalDirective {
-  private regex: RegExp = new RegExp(/^\d{0,10}\.?\d{0,2}$/g);
+  private regex: RegExp = new RegExp(/^\d{1,8}\.?\d{0,3}$/g);
   private specialKeys: Array<string> = ['Backspace', 'Tab', 'End', 'Home', '-', 'ArrowLeft', 'ArrowRight', 'Del', 'Delete'];
-  constructor(private el: ElementRef) { }
-
+  constructor(private el: ElementRef) {
+  }
   @HostListener('keydown', ['$event'])
   onKeyDown(event: KeyboardEvent) {
-    console.log(this.el.nativeElement.value);
     // Allow Backspace, tab, end, and home keys
     if (this.specialKeys.indexOf(event.key) !== -1) {
       return;
     }
     let current: string = this.el.nativeElement.value;
-    let next: string = current.concat(event.key);
+    const position = this.el.nativeElement.selectionStart;
+    const next: string = [current.slice(0, position), event.key == 'Decimal' ? '.' : event.key, current.slice(position)].join('');
     if (next && !String(next).match(this.regex)) {
       event.preventDefault();
     }
   }
-
 }
