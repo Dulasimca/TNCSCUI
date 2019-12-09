f025510a958198fddd926fc3aba4c945a525bd15
Your Name <subashvishal64@gmail.com>
2019-12-09 12:49:46 +0530

    Changes done to Issuer Master Update for the insertion to be done by IssuerNo

--- src/app/constants/tableconstants.ts
+++ src/app/constants/tableconstants.ts
@@ -280,7 +280,7 @@ export class TableConstants {
     this.StockReceiptRegisterReport = [
       { field: 'SlNo', header: 'S.No' },
       { field: 'Ackno', header: 'ACK.No' },
-      { field: 'Godownname', header: 'Godown Name'},
+      { field: 'Godownname', header: 'Godown Name' },
       { field: 'Date', header: 'Date' },
       { field: 'TruckMemoNo', header: 'Truck Memo No' },
       { field: 'Lorryno', header: 'Lorry No' },
@@ -295,7 +295,7 @@ export class TableConstants {
     this.StockIssueRegisterReport = [
       { field: 'SlNo', header: 'S.No' },
       { field: 'Issue_Memono', header: 'Issue Memo No' },
-      { field: 'Godownname', header: 'Godown Name'},
+      { field: 'Godownname', header: 'Godown Name' },
       { field: 'DNo', header: 'Do No' },
       { field: 'Issue_Date', header: 'Date' },
       { field: 'Lorryno', header: 'Lorry No' },
@@ -310,7 +310,7 @@ export class TableConstants {
     this.TruckMemoRegisterReport = [
       { field: 'SlNo', header: 'S.No' },
       { field: 'Truck_Memono', header: 'Truck Memo No' },
-      { field: 'Godownname', header: 'Godown Name'},
+      { field: 'Godownname', header: 'Godown Name' },
       { field: 'Mono', header: 'Mo.No' },
       { field: 'Issue_Date', header: 'Date' },
       { field: 'RoNo', header: 'Ro.No' },
@@ -325,7 +325,7 @@ export class TableConstants {
     this.DeliveryMemoRegisterReport = [
       { field: 'SlNo', header: 'S.No' },
       { field: 'Dono', header: 'Delivery Order No' },
-      { field: 'Godown', header: 'Godown Name'},
+      { field: 'Godown', header: 'Godown Name' },
       { field: 'DeliveryOrderDate', header: 'Date' },
       { field: 'Totals', header: 'Total (Rs)' },
       { field: 'To_Whom_Issued', header: 'Issuer Name' },
@@ -637,7 +637,7 @@ export class TableConstants {
     this.ReceiptROPurchaseReport = [
       { field: 'SlNo', header: 'S.No' },
       { field: 'Ackno', header: 'ACK.NO' },
-      { field: 'Godownname', header: 'Godown Name'},
+      { field: 'Godownname', header: 'Godown Name' },
       { field: 'Date', header: 'DATE' },
       { field: 'Type', header: 'TYPE ' },
       { field: 'Depositor', header: 'DEPOSITOR' },
@@ -652,7 +652,7 @@ export class TableConstants {
     this.ReceiptHOPurchaseReport = [
       { field: 'SlNo', header: 'S.No' },
       { field: 'Ackno', header: 'Ack No' },
-      { field: 'Godownname', header: 'Godown Name'},
+      { field: 'Godownname', header: 'Godown Name' },
       { field: 'Date', header: 'Date' },
       { field: 'Type', header: 'Type' },
       { field: 'Depositor', header: 'Depositor' },
@@ -667,7 +667,7 @@ export class TableConstants {
     this.ReceiptRONOPurchaseReport = [
       { field: 'SlNo', header: 'S.No' },
       { field: 'Ackno', header: 'ACK.NO' },
-      { field: 'Godownname', header: 'Godown Name'},
+      { field: 'Godownname', header: 'Godown Name' },
       { field: 'Date', header: 'DATE' },
       { field: 'Tyname', header: 'TYPE ' },
       { field: 'Depositor', header: 'DEPOSITOR' },
@@ -1016,6 +1016,7 @@ export class TableConstants {
 
     this.IssuerMaster = [
       { header: 'S.No', field: 'SlNo', width: '40px' },
+      { field: 'IssuerNo', header: 'Issuer No' },
       { field: 'IssuerCode', header: 'Issuer Code' },
       { field: 'Tyname', header: 'Type Name' },
       { field: 'SocietyName', header: 'Society Name' },
@@ -1335,7 +1336,7 @@ export class TableConstants {
       { field: 'AllotmentQty', header: 'Allotment Quantity' },
       { field: 'IssueQty', header: 'Issue Quantity' }
     ];
-    
+
     this.ProcessToG2GIssueCols = [
       { header: 'S.No', field: 'SlNo' },
       { header: 'Stock Issue No', field: 'SINo' },
@@ -1387,10 +1388,10 @@ export class TableConstants {
     this.StackCardDocDetailsCols = [
       { header: 'S.No', field: 'SlNo' },
       { header: 'Doc.No', field: 'DocNo' },
-      { header: 'Party Name', field: 'ReceivedFrom'},
+      { header: 'Party Name', field: 'ReceivedFrom' },
       { header: 'Lorry No.', field: 'LorryNo' },
       { header: 'Doc.created Date', field: 'CreatedDate' },
-      { header: 'Bags', field: 'NOOfPACKING'},
+      { header: 'Bags', field: 'NOOfPACKING' },
       { header: 'Quantity', field: 'NETWT' },
     ];
 
