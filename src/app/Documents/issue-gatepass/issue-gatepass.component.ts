f275b2c5a73f9059ff14287146dc7267988212f9
chandrika <udhayachandrikam31@gmail.com>
2020-02-14 16:38:55 +0530

    changes in issue memo gate pass

--- src/app/Documents/issue-gatepass/issue-gatepass.component.ts
+++ src/app/Documents/issue-gatepass/issue-gatepass.component.ts
@@ -33,11 +33,12 @@ export class IssueGatepassComponent implements OnInit {
   viewPane: boolean;
   SelectedGatePassNo: any;
   gatePassNoList: SelectItem[];
+  loading: boolean;
 
   constructor(private restAPIService: RestAPIService, private messageService: MessageService,
     private authService: AuthService, private tableConstants: TableConstants, private datepipe: DatePipe) {
     this.canShowMenu = (this.authService.isLoggedIn()) ? this.authService.isLoggedIn() : false;
-   }
+  }
 
   ngOnInit() {
     this.issueMemoLorryAbstractCols = this.tableConstants.IssueMemoLorryAbstractColumns;
@@ -80,7 +81,7 @@ export class IssueGatepassComponent implements OnInit {
             array[x.LorryNo].forEach(i => {
               value += i + '~';
             })
-           value = value.slice(0, value.length - 1);
+            value = value.slice(0, value.length - 1);
           }
           gropuingArr.push({ label: x.LorryNo, value: value, gatepassNo: x.GatePassId })
         })
@@ -89,14 +90,14 @@ export class IssueGatepassComponent implements OnInit {
         var DocNo = Array.from(new Set(gropuingArr.map((item: any) => item.value)));
         var gId = Array.from(new Set(gropuingArr.map((item: any) => item.gatepassNo)));
         for (var index in LorryNo && DocNo) {
-        issueLorrySelection.push({ label: LorryNo[index], value: DocNo[index], gatePassID: gId[index] });
+          issueLorrySelection.push({ label: LorryNo[index], value: DocNo[index], gatePassID: gId[index] });
         }
         //End
         this.issueLorryNoList = issueLorrySelection;
         this.issueLorryNoList.unshift({ label: '-select-', value: null });
       } else {
-         this.issueLorryNoList = [];
-         this.messageService.clear();
+        this.issueLorryNoList = [];
+        this.messageService.clear();
         this.messageService.add({ key: 't-err', severity: StatusMessage.SEVERITY_WARNING, summary: StatusMessage.SUMMARY_WARNING, detail: StatusMessage.NoRecordMessage });
       }
     }, (err: HttpErrorResponse) => {
@@ -112,47 +113,54 @@ export class IssueGatepassComponent implements OnInit {
   }
 
   onChangeLorryNo() {
-    if(this.SelectedLorryNo !== null && this.SelectedLorryNo !== undefined 
-      && this.SelectedLorryNo.value !== undefined && this.SelectedLorryNo.value !== null)  {
-    const params = {
-      'DocNumber':this.SelectedLorryNo.value,
-      'GName': this.godownName,
-      'RName': this.regionName,
-      'GCode': this.GCode,
-      'GatePassNo': this.SelectedLorryNo.gatePassID,
-      'UserID': this.userId.user,
-      'Type': 1
-    };
-    this.restAPIService.post(PathConstants.STOCK_ISSUE_GATEPASS_POST, params).subscribe((res: any) => {
-      if(res.Item3.length !== 0 && res.Item3 !== null && res.Item3 !== undefined) {
-        var data = JSON.parse(res.Item3);
-        var obtainDistinctData = Array.from(new Set(data.map((item: any) => item)));
-        this.issueMemoLorryAbstractData = obtainDistinctData;
-        let sno = 1;
-        this.issueMemoLorryAbstractData.forEach(x => {
-          x.SlNo = sno;
-          x.SIDate = this.datepipe.transform(x.SIDate, 'dd/MM/yyyy');
-          sno += 1;
-        })
-      } else {
+    if (this.SelectedLorryNo !== null && this.SelectedLorryNo !== undefined
+      && this.SelectedLorryNo.value !== undefined && this.SelectedLorryNo.value !== null) {
+      this.loading = true;
+      const params = {
+        'DocNumber': this.SelectedLorryNo.value,
+        'GName': this.godownName,
+        'RName': this.regionName,
+        'GCode': this.GCode,
+        'GatePassNo': this.SelectedLorryNo.gatePassID,
+        'UserID': this.userId.user,
+        'Type': 1
+      };
+      this.restAPIService.post(PathConstants.STOCK_ISSUE_GATEPASS_POST, params).subscribe((res: any) => {
+        if (res.Item3.length !== 0 && res.Item3 !== null && res.Item3 !== undefined) {
+          var data = JSON.parse(res.Item3);
+          var obtainDistinctData = Array.from(new Set(data.map((item: any) => item)));
+          this.issueMemoLorryAbstractData = obtainDistinctData;
+          let sno = 1;
+          this.issueMemoLorryAbstractData.forEach(x => {
+            x.SlNo = sno;
+            x.SIDate = this.datepipe.transform(x.SIDate, 'dd/MM/yyyy');
+            sno += 1;
+          })
+          this.loading = false;
+        } else {
+          this.loading = false;
+          this.issueMemoLorryAbstractData = [];
+          this.messageService.clear();
+          this.messageService.add({ key: 't-err', severity: StatusMessage.SEVERITY_WARNING, summary: StatusMessage.SUMMARY_WARNING, detail: StatusMessage.NoRecordMessage });
+        }
+      }, (err: HttpErrorResponse) => {
+        this.loading = false;
         this.issueMemoLorryAbstractData = [];
-        this.messageService.clear();
-        this.messageService.add({ key: 't-err', severity: StatusMessage.SEVERITY_WARNING, summary: StatusMessage.SUMMARY_WARNING, detail: StatusMessage.NoRecordMessage });
-      }
-    }, (err: HttpErrorResponse) => {
+        if (err.status === 0 || err.status === 400) {
+          this.messageService.clear();
+          this.messageService.add({ key: 't-err', severity: StatusMessage.SEVERITY_ERROR, summary: StatusMessage.SUMMARY_ERROR, detail: StatusMessage.ErrorMessage });
+        } else {
+          this.messageService.clear();
+          this.messageService.add({ key: 't-err', severity: StatusMessage.SEVERITY_ERROR, summary: StatusMessage.SUMMARY_ERROR, detail: err.message });
+        }
+      });
+    } else {
+      this.loading = false;
       this.issueMemoLorryAbstractData = [];
-      if (err.status === 0 || err.status === 400) {
-        this.messageService.clear();
-        this.messageService.add({ key: 't-err', severity: StatusMessage.SEVERITY_ERROR, summary: StatusMessage.SUMMARY_ERROR, detail: StatusMessage.ErrorMessage });
-      } else {
-        this.messageService.clear();
-        this.messageService.add({ key: 't-err', severity: StatusMessage.SEVERITY_ERROR, summary: StatusMessage.SUMMARY_ERROR, detail: err.message });
-      }
-    });
-  }
+    }
   }
 
-  onView() { 
+  onView() {
     this.viewPane = true;
     this.issueGatePassCols = this.tableConstants.IssueMemoLorryAbstractColumns;
     this.onDateChanges();
@@ -161,10 +169,10 @@ export class IssueGatepassComponent implements OnInit {
   onDateChanges() {
     let issueGatePassSelection = [];
     let distinctArr = [];
-    if(this.viewDate !== null && this.viewDate !== undefined && this.viewDate.toDateString() !== '') {
-    const params = new HttpParams().set('GCode', this.GCode).append('DocDate', this.datepipe.transform(this.viewDate, 'MM/dd/yyyy'));
-    this.restAPIService.getByParameters(PathConstants.ISSUE_MEMO_GATE_PASS_GET, params).subscribe(res => {
-      if(res.length !== 0 && res !== null && res !== undefined) {
+    if (this.viewDate !== null && this.viewDate !== undefined && this.viewDate.toDateString() !== '') {
+      const params = new HttpParams().set('GCode', this.GCode).append('DocDate', this.datepipe.transform(this.viewDate, 'MM/dd/yyyy'));
+      this.restAPIService.getByParameters(PathConstants.ISSUE_MEMO_GATE_PASS_GET, params).subscribe(res => {
+        if (res.length !== 0 && res !== null && res !== undefined) {
           // construct object of unique values with keys
           let formObject = {};
           for (var i = 0; i < res.length; i++) {
@@ -188,7 +196,7 @@ export class IssueGatepassComponent implements OnInit {
               array[x.GatePassId].forEach(i => {
                 value += i + '~';
               })
-             value = value.slice(0, value.length - 1);
+              value = value.slice(0, value.length - 1);
             }
             distinctArr.push({ label: x.GatePassId, value: value })
           })
@@ -201,82 +209,89 @@ export class IssueGatepassComponent implements OnInit {
           //End
           this.gatePassNoList = issueGatePassSelection;
           this.gatePassNoList.unshift({ label: '-select-', value: null });
-      } else {
-        this.messageService.clear();
-        this.messageService.add({ key: 't-err', severity: StatusMessage.SEVERITY_WARNING, summary: StatusMessage.SUMMARY_WARNING, detail: StatusMessage.NoGatePassFound });
-      }
-    }, (err: HttpErrorResponse) => {
-      if (err.status === 0 || err.status === 400) {
-        this.messageService.clear();
-        this.messageService.add({ key: 't-err', severity: StatusMessage.SEVERITY_ERROR, summary: StatusMessage.SUMMARY_ERROR, detail: StatusMessage.ErrorMessage });
-      } else {
-        this.messageService.clear();
-        this.messageService.add({ key: 't-err', severity: StatusMessage.SEVERITY_ERROR, summary: StatusMessage.SUMMARY_ERROR, detail: err.message });
-      }
-    });
-  }
+        } else {
+          this.messageService.clear();
+          this.messageService.add({ key: 't-err', severity: StatusMessage.SEVERITY_WARNING, summary: StatusMessage.SUMMARY_WARNING, detail: StatusMessage.NoGatePassFound });
+        }
+      }, (err: HttpErrorResponse) => {
+        if (err.status === 0 || err.status === 400) {
+          this.messageService.clear();
+          this.messageService.add({ key: 't-err', severity: StatusMessage.SEVERITY_ERROR, summary: StatusMessage.SUMMARY_ERROR, detail: StatusMessage.ErrorMessage });
+        } else {
+          this.messageService.clear();
+          this.messageService.add({ key: 't-err', severity: StatusMessage.SEVERITY_ERROR, summary: StatusMessage.SUMMARY_ERROR, detail: err.message });
+        }
+      });
+    }
   }
 
   onChangeGatePassNo() {
-    if(this.SelectedGatePassNo !== null && this.SelectedGatePassNo !== undefined 
-      && this.SelectedGatePassNo.value !== undefined && this.SelectedGatePassNo.value !== null)  {
-    const params = {
-      'DocNumber':this.SelectedGatePassNo.value,
-      'GName': this.godownName,
-      'RName': this.regionName,
-      'GCode': this.GCode,
-      'GatePassNo': this.SelectedGatePassNo.label,
-      'UserID': this.userId.user,
-      'Type': 2
-    };
-    this.restAPIService.post(PathConstants.STOCK_ISSUE_GATEPASS_POST, params).subscribe((res: any) => {
-      if(res.Item3.length !== 0 && res.Item3 !== null && res.Item3 !== undefined) {
-        var data = JSON.parse(res.Item3);
-        var distinctArr = Array.from(new Set(data.map((item: any) => item)));
-        this.issueGatePassData = distinctArr;
-        let sno = 1;
-        this.issueGatePassData.forEach(x => {
-          x.SlNo = sno;
-          x.SIDate = this.datepipe.transform(x.SIDate, 'dd/MM/yyyy');
-          sno += 1;
-        })
-      } else {
+    if (this.SelectedGatePassNo !== null && this.SelectedGatePassNo !== undefined
+      && this.SelectedGatePassNo.value !== undefined && this.SelectedGatePassNo.value !== null) {
+      this.loading = true;
+      const params = {
+        'DocNumber': this.SelectedGatePassNo.value,
+        'GName': this.godownName,
+        'RName': this.regionName,
+        'GCode': this.GCode,
+        'GatePassNo': this.SelectedGatePassNo.label,
+        'UserID': this.userId.user,
+        'Type': 2
+      };
+      this.restAPIService.post(PathConstants.STOCK_ISSUE_GATEPASS_POST, params).subscribe((res: any) => {
+        if (res.Item3.length !== 0 && res.Item3 !== null && res.Item3 !== undefined) {
+          var data = JSON.parse(res.Item3);
+          var distinctArr = Array.from(new Set(data.map((item: any) => item)));
+          this.issueGatePassData = distinctArr;
+          let sno = 1;
+          this.issueGatePassData.forEach(x => {
+            x.SlNo = sno;
+            x.SIDate = this.datepipe.transform(x.SIDate, 'dd/MM/yyyy');
+            sno += 1;
+          })
+          this.loading = false;
+        } else {
+          this.loading = false;
+          this.issueGatePassData = [];
+          this.messageService.clear();
+          this.messageService.add({ key: 't-err', severity: StatusMessage.SEVERITY_WARNING, summary: StatusMessage.SUMMARY_WARNING, detail: StatusMessage.NoRecordMessage });
+        }
+      }, (err: HttpErrorResponse) => {
+        this.loading = false;
         this.issueGatePassData = [];
-        this.messageService.clear();
-        this.messageService.add({ key: 't-err', severity: StatusMessage.SEVERITY_WARNING, summary: StatusMessage.SUMMARY_WARNING, detail: StatusMessage.NoRecordMessage });
-      }
-    }, (err: HttpErrorResponse) => {
+        if (err.status === 0 || err.status === 400) {
+          this.messageService.clear();
+          this.messageService.add({ key: 't-err', severity: StatusMessage.SEVERITY_ERROR, summary: StatusMessage.SUMMARY_ERROR, detail: StatusMessage.ErrorMessage });
+        } else {
+          this.messageService.clear();
+          this.messageService.add({ key: 't-err', severity: StatusMessage.SEVERITY_ERROR, summary: StatusMessage.SUMMARY_ERROR, detail: err.message });
+        }
+      });
+    } else {
+      this.loading = false;
       this.issueGatePassData = [];
-      if (err.status === 0 || err.status === 400) {
-        this.messageService.clear();
-        this.messageService.add({ key: 't-err', severity: StatusMessage.SEVERITY_ERROR, summary: StatusMessage.SUMMARY_ERROR, detail: StatusMessage.ErrorMessage });
-      } else {
-        this.messageService.clear();
-        this.messageService.add({ key: 't-err', severity: StatusMessage.SEVERITY_ERROR, summary: StatusMessage.SUMMARY_ERROR, detail: err.message });
-      }
-    });
-  }
+    }
   }
 
   onPrintAbstract(type) {
-    if(type === '1') {
-    const params = { 
-      'GatePassNo': this.SelectedLorryNo.gatePassID,
-      'GCode': this.GCode
+    if (type === '1') {
+      const params = {
+        'GatePassNo': this.SelectedLorryNo.gatePassID,
+        'GCode': this.GCode
+      }
+      const path = "../../assets/Reports/" + this.userId.user + "/";
+      const filename = this.GCode + GolbalVariable.IssueMemoGatePass;
+      let filepath = path + filename + ".txt";
+      var w = window.open(filepath);
+      w.print();
+      this.restAPIService.put(PathConstants.STOCK_ISSUE_GATEPASS_PUT, params).subscribe();
+    } else {
+      const path = "../../assets/Reports/" + this.userId.user + "/";
+      const filename = this.GCode + GolbalVariable.IssueMemoGatePass;
+      let filepath = path + filename + ".txt";
+      var w = window.open(filepath);
+      w.print();
     }
-    const path = "../../assets/Reports/" + this.userId.user + "/";
-    const filename = this.GCode + GolbalVariable.IssueMemoGatePass;
-    let filepath = path + filename + ".txt";
-    var w = window.open(filepath);
-    w.print();
-    this.restAPIService.put(PathConstants.STOCK_ISSUE_GATEPASS_PUT, params).subscribe();
-  } else {
-    const path = "../../assets/Reports/" + this.userId.user + "/";
-    const filename = this.GCode + GolbalVariable.IssueMemoGatePass;
-    let filepath = path + filename + ".txt";
-    var w = window.open(filepath);
-    w.print();
-  }
   }
 
   onClose() {
