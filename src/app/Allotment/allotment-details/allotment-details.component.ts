01861f130ed96fedc5e26c088e9e876699f87fb2
chandrika <udhayachandrikam31@gmail.com>
2020-11-08 23:10:18 +0530

    allotment details excel code modifications

--- src/app/Allotment/allotment-details/allotment-details.component.ts
+++ src/app/Allotment/allotment-details/allotment-details.component.ts
@@ -55,6 +55,7 @@ export class AllotmentDetailsComponent implements OnInit {
   @ViewChild('y', { static: false }) yearPanel: Dropdown;
   @ViewChild('fileSelector', { static: false }) fileSelector: ElementRef;
   @ViewChild('dt', { static: false }) table: Table;
+  cell_range: number;
 
   constructor(private authService: AuthService, private datepipe: DatePipe, private restAPIService: RestAPIService,
     private messageService: MessageService, private roleBasedService: RoleBasedService,
@@ -114,88 +115,88 @@ export class AllotmentDetailsComponent implements OnInit {
         { 'label': 'Nov', 'value': '11' }, { 'label': 'Dec', 'value': '12' }];
         this.monthOptions.unshift({ 'label': '-select-', 'value': null });
         break;
-        case 'reg':
-          this.regions = this.roleBasedService.regionsData;
-          if (type === 'tab') {
-            this.regionPanel.overlayVisible = true;
+      case 'reg':
+        this.regions = this.roleBasedService.regionsData;
+        if (type === 'tab') {
+          this.regionPanel.overlayVisible = true;
+        }
+        if (this.roleId === 1) {
+          if (this.regions !== undefined) {
+            this.regions.forEach(x => {
+              regionSelection.push({ 'label': x.RName, 'value': x.RCode });
+            });
+            this.regionOptions = regionSelection;
           }
-          if (this.roleId === 1) {
-            if (this.regions !== undefined) {
-              this.regions.forEach(x => {
+        } else {
+          if (this.regions !== undefined) {
+            this.regions.forEach(x => {
+              if (x.RCode === this.loggedInRCode) {
                 regionSelection.push({ 'label': x.RName, 'value': x.RCode });
-              });
-              this.regionOptions = regionSelection;
-            }
-          } else {
-            if (this.regions !== undefined) {
-              this.regions.forEach(x => {
-                if (x.RCode === this.loggedInRCode) {
-                  regionSelection.push({ 'label': x.RName, 'value': x.RCode });
-                }
-              });
-              this.regionOptions = regionSelection;
-            }
-          }
-          break;
-        case 'gd':
-          if (type === 'tab') {
-            this.godownPanel.overlayVisible = true;
-          }
-          if (this.data !== undefined) {
-            this.data.forEach(x => {
-              if (x.RCode === this.RCode) {
-                godownSelection.push({ 'label': x.GName, 'value': x.GCode, 'rcode': x.RCode, 'rname': x.RName });
               }
             });
-            this.godownOptions = godownSelection;
+            this.regionOptions = regionSelection;
+          }
+        }
+        break;
+      case 'gd':
+        if (type === 'tab') {
+          this.godownPanel.overlayVisible = true;
+        }
+        if (this.data !== undefined) {
+          this.data.forEach(x => {
+            if (x.RCode === this.RCode) {
+              godownSelection.push({ 'label': x.GName, 'value': x.GCode, 'rcode': x.RCode, 'rname': x.RName });
+            }
+          });
+          this.godownOptions = godownSelection;
           break;
+        }
     }
-  }
   }
 
   getAllotmentDetails() {
     this.table.reset();
-    if(this.GCode !== undefined && this.GCode !== null && this.month !== null && this.month !== undefined
-     && ((this.month.value !== undefined && this.month.value !== null) 
-     || (this.curMonth !== undefined && this.curMonth !== null)) && this.year !== null 
-     && this.year !== undefined && this.curMonth !== null && this.curMonth !== undefined) {
-    const params = new HttpParams().set('GCode', this.GCode)
-    .append('AMonth', (this.month.value !== undefined && this.month.value !== null) ? this.month.value : this.curMonth)
-    .append('AYear', this.year);
-    this.loading = true;
-    this.table.reset();
-    this.AllotmentData = [];
-    this.AllotmentCols = [];
-    this.totalRecords = 0;
-    this.restAPIService.getByParameters(PathConstants.ALLOTMENT_BALANCE_GET, params).subscribe(res => {
-      if(res.length !== 0 && res !== undefined && res !== null) {
-        this.AllotmentCols = this.tableConstants.AllotmentDetailsCols;
-        let sno = 1;
-        res.forEach(x => {
-          x.SlNo = sno;
-          x.Quantity = (x.Quantity * 1).toFixed(3);
-          sno += 1;
-        })
-        this.AllotmentData = res;
+    if (this.GCode !== undefined && this.GCode !== null && this.month !== null && this.month !== undefined
+      && ((this.month.value !== undefined && this.month.value !== null)
+        || (this.curMonth !== undefined && this.curMonth !== null)) && this.year !== null
+      && this.year !== undefined && this.curMonth !== null && this.curMonth !== undefined) {
+      const params = new HttpParams().set('GCode', this.GCode)
+        .append('AMonth', (this.month.value !== undefined && this.month.value !== null) ? this.month.value : this.curMonth)
+        .append('AYear', this.year);
+      this.loading = true;
+      this.table.reset();
+      this.AllotmentData = [];
+      this.AllotmentCols = [];
+      this.totalRecords = 0;
+      this.restAPIService.getByParameters(PathConstants.ALLOTMENT_BALANCE_GET, params).subscribe(res => {
+        if (res.length !== 0 && res !== undefined && res !== null) {
+          this.AllotmentCols = this.tableConstants.AllotmentDetailsCols;
+          let sno = 1;
+          res.forEach(x => {
+            x.SlNo = sno;
+            x.Quantity = (x.Quantity * 1).toFixed(3);
+            sno += 1;
+          })
+          this.AllotmentData = res;
+          this.loading = false;
+          this.disableSave = true;
+        } else {
+          this.loading = false;
+          this.messageService.clear();
+          this.messageService.add({ key: 't-err', severity: StatusMessage.SEVERITY_WARNING, summary: StatusMessage.SUMMARY_WARNING, detail: StatusMessage.NoRecForCombination });
+        }
+      }, (err: HttpErrorResponse) => {
+        this.disableSave = false;
         this.loading = false;
-        this.disableSave = true;
-      } else {
-        this.loading = false;
-        this.messageService.clear();
-        this.messageService.add({ key: 't-err', severity: StatusMessage.SEVERITY_WARNING, summary: StatusMessage.SUMMARY_WARNING, detail: StatusMessage.NoRecForCombination });
-      }
-    }, (err: HttpErrorResponse) => {
-      this.disableSave = false;
-      this.loading = false;
-      if (err.status === 0 || err.status === 400) {
-        this.messageService.clear();
-        this.messageService.add({ key: 't-err', severity: StatusMessage.SEVERITY_ERROR, summary: StatusMessage.SUMMARY_ERROR, detail: StatusMessage.ErrorMessage });
-      } else {
-        this.messageService.clear();
-        this.messageService.add({ key: 't-err', severity: StatusMessage.SEVERITY_ERROR, summary: StatusMessage.SUMMARY_ERROR, detail: StatusMessage.NetworkErrorMessage });
-      }
-    });
-  }
+        if (err.status === 0 || err.status === 400) {
+          this.messageService.clear();
+          this.messageService.add({ key: 't-err', severity: StatusMessage.SEVERITY_ERROR, summary: StatusMessage.SUMMARY_ERROR, detail: StatusMessage.ErrorMessage });
+        } else {
+          this.messageService.clear();
+          this.messageService.add({ key: 't-err', severity: StatusMessage.SEVERITY_ERROR, summary: StatusMessage.SUMMARY_ERROR, detail: StatusMessage.NetworkErrorMessage });
+        }
+      });
+    }
   }
 
   onSort() {
@@ -212,7 +213,7 @@ export class AllotmentDetailsComponent implements OnInit {
     this.AllotmentData.length = 0;
     this.blockScreen = true;
     let filesData = event.target.files;
-    if(this.GCode !== undefined && this.GCode !== null) {
+    if (this.GCode !== undefined && this.GCode !== null) {
       const params = { 'Type': 2, 'GCode': this.GCode }
       this.restAPIService.getByParameters(PathConstants.ISSUER_MASTER_GET, params).subscribe(data => {
         this.societyData = data.filter(x => {
@@ -227,8 +228,10 @@ export class AllotmentDetailsComponent implements OnInit {
         } else {
           this.blockScreen = false;
           this.messageService.clear();
-          this.messageService.add({ key: 't-err', severity: StatusMessage.SEVERITY_ERROR, summary: StatusMessage.SUMMARY_ERROR,
-          life:5000, detail: this.errMsg });
+          this.messageService.add({
+            key: 't-err', severity: StatusMessage.SEVERITY_ERROR, summary: StatusMessage.SUMMARY_ERROR,
+            life: 5000, detail: this.errMsg
+          });
         }
       })
     }
@@ -244,10 +247,26 @@ export class AllotmentDetailsComponent implements OnInit {
         type: 'binary'
       });
       let sheetName: any = workbook.SheetNames[0];
-        let XL_row_object = XLSX.utils.sheet_to_json(workbook.Sheets[sheetName]);
-        let headers = get_header_row(workbook.Sheets[sheetName]);
-        let isValid = this.checkValidHeaders(headers);
-        if(isValid.result) {
+      let worksheet: XLSX.WorkSheet = workbook.Sheets[workbook.SheetNames[0]];
+      // removing unwanted rows from imported excel
+      for (let i = 1; i <= 15; i++) {
+        if (worksheet['A' + i] !== undefined && (worksheet['A' + i].t === 's' && worksheet['A' + i].v === '#')) {
+          this.cell_range = i;
+          break;
+        } else {
+          delete worksheet['A' + i];
+          continue;
+        }
+      }
+      //setting worksheet ref number according to imported excel
+      var ref_len = worksheet["!ref"].length;
+      var ref = 'A' + this.cell_range + worksheet["!ref"].slice(2, ref_len);
+      worksheet["!ref"] = ref;
+
+      let XL_row_object = XLSX.utils.sheet_to_json(worksheet, { blankrows: false });
+      let headers = get_header_row(worksheet, this.cell_range - 1);
+      let isValid = this.checkValidHeaders(headers);
+      if (isValid.result) {
         headers.forEach(c => {
           this.AllotmentCols.push({ header: c, field: c, width: '180px' });
         })
@@ -295,24 +314,28 @@ export class AllotmentDetailsComponent implements OnInit {
         } else {
           this.blockScreen = false;
           this.messageService.clear();
-          this.messageService.add({ key: 't-err', severity: StatusMessage.SEVERITY_ERROR, summary: StatusMessage.SUMMARY_ERROR, 
-          life:5000, detail: StatusMessage.GodownCodeMismatch });
+          this.messageService.add({
+            key: 't-err', severity: StatusMessage.SEVERITY_ERROR, summary: StatusMessage.SUMMARY_ERROR,
+            life: 5000, detail: StatusMessage.GodownCodeMismatch
+          });
         }
+      } else {
+        let missingFields: string = '';
+        this.blockScreen = false;
+        if (isValid.res.length > 1) {
+          isValid.res.forEach((x, index) => {
+            missingFields += (index + 1) + '.' + x.toUpperCase() + ' ';
+          })
+          missingFields += ' columns are missing!';
         } else {
-          let missingFields: string = '';
-          this.blockScreen = false;
-          if(isValid.res.length > 1) {
-            isValid.res.forEach((x, index) => {
-              missingFields += (index + 1) + '.' + x.toUpperCase() + ' ';
-            })
-            missingFields += ' columns are missing!';
-          } else { 
-            missingFields = isValid.res[0].toUpperCase() + ' column is missing!'; 
-          }
-          this.messageService.clear();
-          this.messageService.add({ key: 't-err', severity: StatusMessage.SEVERITY_ERROR, summary: StatusMessage.SUMMARY_ERROR, 
-          life:5000, detail: missingFields });
+          missingFields = isValid.res[0].toUpperCase() + ' column is missing!';
         }
+        this.messageService.clear();
+        this.messageService.add({
+          key: 't-err', severity: StatusMessage.SEVERITY_ERROR, summary: StatusMessage.SUMMARY_ERROR,
+          life: 5000, detail: missingFields
+        });
+      }
     };
 
     reader.onerror = function (ex) {
@@ -324,16 +347,16 @@ export class AllotmentDetailsComponent implements OnInit {
   checkValidHeaders(headers): any {
     let result: boolean;
     let tempArr = ['Taluk', 'FPS Code', 'FPS Name', 'Godown Name', 'Godown Code'];
-    if(headers !== undefined && headers !== null && headers.length !== 0) {
-    let res =  tempArr.filter(f => !headers.includes(f));
-      if(res.length !== 0) {
+    if (headers !== undefined && headers !== null && headers.length !== 0) {
+      let res = tempArr.filter(f => !headers.includes(f));
+      if (res.length !== 0) {
         result = false;
       } else {
         result = true;
       }
-    return { result, res }
+      return { result, res }
+    }
   }
-}
 
   constructData(data) {
     let records = 1;
@@ -350,8 +373,12 @@ export class AllotmentDetailsComponent implements OnInit {
     this.blockScreen = false;
   }
 
+  set_cell_range(range) {
+    this.cell_range = range;
+  }
+
   onResetTable(item) {
-    if(item === 'reg') { 
+    if (item === 'reg') {
       this.GCode = null;
     }
     this.AllotmentData = [];
@@ -366,9 +393,9 @@ export class AllotmentDetailsComponent implements OnInit {
     this.restAPIService.post(PathConstants.ALLOTMENT_QUANTITY_POST, this.allotmentDetails).subscribe((res: any) => {
       if (res.Item1) {
         this.count += 1;
-        if(this.count <= 1) {
+        if (this.count <= 1) {
           this.allotmentDetails.forEach(x => {
-            if((x.AllotmentMonth * 1) === 12) {
+            if ((x.AllotmentMonth * 1) === 12) {
               x.AllotmentMonth = '01';
               x.AllotmentYear = (x.AllotmentYear * 1) + 1;
             } else {
@@ -376,11 +403,11 @@ export class AllotmentDetailsComponent implements OnInit {
               x.AllotmentMonth = ((AllotmentMonth * 1) <= 9) ? '0' + AllotmentMonth : AllotmentMonth;
             }
           })
-        this.onSave();
-        this.blockScreen = false;
-        this.onClear();
-        this.messageService.clear();
-        this.messageService.add({ key: 't-err', severity: StatusMessage.SEVERITY_SUCCESS, summary: StatusMessage.SUMMARY_SUCCESS, detail: res.Item2 });
+          this.onSave();
+          this.blockScreen = false;
+          this.onClear();
+          this.messageService.clear();
+          this.messageService.add({ key: 't-err', severity: StatusMessage.SEVERITY_SUCCESS, summary: StatusMessage.SUMMARY_SUCCESS, detail: res.Item2 });
         }
       } else {
         this.blockScreen = false;
@@ -424,7 +451,7 @@ export class AllotmentDetailsComponent implements OnInit {
     const EXCEL_TYPE = 'application/vnd.openxmlformats-officedocument.spreadsheethtml.sheet;charset=UTF-8';
     const path = "../../assets/Sample_Allotment/Sample_Excel.xlsx";
     const filename = 'Sample_Excel' + ".xlsx";
-    saveAs(path , filename);
+    saveAs(path, filename);
   }
 
   onClose() {
@@ -438,20 +465,20 @@ function checkfile(sender) {
   var fileExt = fileName.substring(fileName.lastIndexOf('.'));
   if (validExts.indexOf(fileExt) < 0) {
     this.errMsg = "Invalid file selected, valid files are of " +
-    validExts.toString() + " types.";
+      validExts.toString() + " types.";
     return false;
   }
   else return true;
 }
 
-function get_header_row(sheet) {
+function get_header_row(sheet, cell_range) {
   var headers = [];
   var range = XLSX.utils.decode_range(sheet['!ref']);
-  var C, R = range.s.r; /* start in the first row */
+  var C = range.s.r;
+  var R = (cell_range !== undefined) ? cell_range : range.s.r; /* start in the first row */
   /* walk every column in the range */
   for (C = range.s.c; C <= range.e.c; ++C) {
     var cell = sheet[XLSX.utils.encode_cell({ c: C, r: R })] /* find the cell in the first row */
-
     var hdr: any = "HEADER " + C; // <-- replace with your desired default 
     if (cell && cell.t) hdr = XLSX.utils.format_cell(cell);
     hdr = hdr.trimRight();
@@ -462,11 +489,33 @@ function get_header_row(sheet) {
 
 function trimObj(obj) {
   if (!Array.isArray(obj) && typeof obj != 'object') return obj;
-  return Object.keys(obj).reduce(function(acc, key) {
-    acc[key.trim()] = typeof obj[key] == 'string'? obj[key].trim() : trimObj(obj[key]);
+  return Object.keys(obj).reduce(function (acc, key) {
+    acc[key.trim()] = typeof obj[key] == 'string' ? obj[key].trim() : trimObj(obj[key]);
     return acc;
-  }, Array.isArray(obj)? []:{});
+  }, Array.isArray(obj) ? [] : {});
 }
+
+function ec(r, c) {
+  return XLSX.utils.encode_cell({ r: r, c: c });
+}
+
+function delete_row(ws, row_index) {
+  var variable = XLSX.utils.decode_range(ws["!ref"])
+  for (var R = row_index; R < variable.e.r; ++R) {
+    for (var C = variable.s.c; C <= variable.e.c; ++C) {
+      ws[ec(R, C)] = ws[ec(R + 1, C)];
+    }
+  }
+  variable.e.r--
+  ws['!ref'] = XLSX.utils.encode_range(variable.s, variable.e);
+}
+
+// function set_cell_range(range) {
+//   let cell_range = range;
+// }
+// function get_cell_range() {
+//   return cell_range;
+// }
 
 export interface Allotment {
   Type: number;
